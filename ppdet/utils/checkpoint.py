b6c5b6f6099b65b6a4db8448e4df52c152a2b059
qingqing01 <dangqingqing@baidu.com>
2019-07-01 20:50:44 +0800

    Add depth for ResNetC5 in some configs (#2655)

--- ppdet/utils/checkpoint.py
+++ ppdet/utils/checkpoint.py
@@ -183,7 +183,8 @@ def load_and_fusebn(exe, prog, path):
                         [scale_name, bias_name, mean_name, variance_name])
 
     if not bn_in_path:
-        raise ValueError("The model in path {} has not params of batch norm.")
+        raise ValueError("There is no params of batch norm in model {}.".format(
+            path))
 
     # load running mean and running variance on cpu place into global scope.
     place = fluid.CPUPlace()
