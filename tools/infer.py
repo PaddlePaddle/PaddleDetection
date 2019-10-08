3ed4fd64df68e9ffd7a618c45fc483b2c49f24cd
wangguanzhong <jerrywgz@126.com>
2019-10-08 14:27:55 +0800

    fix prune interface (#3472)

--- tools/infer.py
+++ tools/infer.py
@@ -106,7 +106,7 @@ def prune_feed_vars(feeded_var_names, target_vars, prog):
     """
     exist_var_names = []
     prog = prog.clone()
-    prog = prog._prune(feeded_var_names, targets=target_vars)
+    prog = prog._prune(targets=target_vars)
     global_block = prog.global_block()
     for name in feeded_var_names:
         try:
