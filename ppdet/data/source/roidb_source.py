a8ae92aa4feb8ff3927a41e60766a789ca0d1c50
Kaipeng Deng <dengkaipeng@baidu.com>
2019-10-13 12:23:28 +0800

    refine assert info in roidb_source.py (#3536)
    * refine assert info in roidb_source.py
    
    * refine image_dir assert

--- ppdet/data/source/roidb_source.py
+++ ppdet/data/source/roidb_source.py
@@ -59,13 +59,13 @@ class RoiDbSource(Dataset):
         """
         super(RoiDbSource, self).__init__()
         self._epoch = -1
-        assert os.path.isfile(anno_file) or os.path.isdir(
-            anno_file), 'invalid file[%s] for RoiDbSource' % (anno_file)
+        assert os.path.isfile(anno_file) or os.path.isdir(anno_file), \
+                'anno_file {} is not a file or a directory'.format(anno_file)
         self._fname = anno_file
         self._image_dir = image_dir
         if image_dir is not None:
-            assert os.path.isdir(image_dir), 'invalid image directory[%s]' % (
-                image_dir)
+            assert os.path.isdir(image_dir), \
+                    'image_dir {} is not a directory'.format(image_dir)
         self._roidb = None
         self._pos = -1
         self._drained = False
