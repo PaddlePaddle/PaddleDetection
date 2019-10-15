97fc087bad7b682a6f5871327a5bf143297f5969
wangguanzhong <jerrywgz@126.com>
2019-10-15 14:05:45 +0800

    refine log when mask is empty (#3578)

--- ppdet/utils/coco_eval.py
+++ ppdet/utils/coco_eval.py
@@ -108,9 +108,10 @@ def mask_eval(results, anno_file, outfile, resolution, thresh_binarize=0.5):
     clsid2catid = {i + 1: v for i, v in enumerate(coco_gt.getCatIds())}
 
     segm_results = mask2out(results, clsid2catid, resolution, thresh_binarize)
-    assert len(
-        segm_results) > 0, "The number of valid mask detected is zero.\n \
-        Please use reasonable model and check input data."
+    if len(segm_results) == 0:
+        logger.warning("The number of valid mask detected is zero.\n \
+            Please use reasonable model and check input data.")
+        return
 
     with open(outfile, 'w') as f:
         json.dump(segm_results, f)
