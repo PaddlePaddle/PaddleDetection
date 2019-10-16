db627af7f86c3eae144f00b1cf5e615214a7204d
wangguanzhong <jerrywgz@126.com>
2019-10-16 21:11:43 +0800

    polish fruit-detection download (#3626)

--- dataset/fruit/download.sh
+++ dataset/fruit/download.sh
@@ -7,8 +7,4 @@ wget https://dataset.bj.bcebos.com/PaddleDetection_demo/fruit-detection.tar
 # Extract the data.
 echo "Extracting..."
 tar xvf fruit-detection.tar
-cd fruit-detection
-tar xvf Annotations.tar
-tar xvf ImageSets.tar
-tar xvf JPEGImages.tar
-rm -rf ./*.tar
+rm -rf fruit-detection.tar
