3eb68e9021b72c4550e2976e5278826b8c3d6f6b
jerrywgz <jerrywgz@126.com>
2019-12-31 08:05:18 +0000

    support use_default_label for infer

--- ppdet/data/source/dataset.py
+++ ppdet/data/source/dataset.py
@@ -120,12 +120,11 @@ class ImageFolder(DataSet):
                  anno_path=None,
                  sample_num=-1,
                  with_background=True,
+                 use_default_label=None,
                  **kwargs):
-        super(ImageFolder, self).__init__(image_dir, anno_path, sample_num,
-                                          dataset_dir, with_background)
-        self.anno_path = anno_path
-        self.sample_num = sample_num
-        self.with_background = with_background
+        super(ImageFolder, self).__init__(dataset_dir, image_dir, anno_path,
+                                          sample_num, with_background,
+                                          use_default_label)
         self.roidbs = None
         self._imid2path = {}
 
