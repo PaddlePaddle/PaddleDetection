8b34c68a5843f4f7dbf025d6bdcec2b899b6dfd8
dengkaipeng <dengkaipeng@baidu.com>
2020-02-06 11:06:52 +0000

    debug for requirement

--- .travis/unittest.sh
+++ .travis/unittest.sh
@@ -6,6 +6,12 @@ abort(){
     exit 1
 }
 
+log(){
+  echo "Run unittest failed" 1>&2
+  echo "\t1. you can run 'bash .travis/unittest.sh locally.'" 1<&2
+  echo "\t2. you can add python requirement in .travis/requirements.txt for ImportError" 1<&2
+}
+
 unittest(){
     if [ $? != 0 ]; then
         exit 1
@@ -15,13 +21,18 @@ unittest(){
         'python -m unittest discover -v -s {}'
 }
 
+# install travis python dependencies
+if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
+fi
+
 trap 'abort' 0
 set -e
 
-# install travis python dependencies
-if [ -f ".travis/requirements.txt" ]; then
-    pip install -r .travis/requirements.txt
-fi
+# # install travis python dependencies
+# if [ -f ".travis/requirements.txt" ]; then
+#     pip install -r .travis/requirements.txt
+# fi
 export PYTHONPATH=`pwd`:$PYTHONPATH
 
 unittest .
