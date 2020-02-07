90d29eb55c87b8a8405eee1c721f041e8048e2cc
dengkaipeng <dengkaipeng@baidu.com>
2020-02-07 13:24:25 +0000

    enable data unittest

--- .travis/unittest.sh
+++ .travis/unittest.sh
@@ -12,7 +12,7 @@ unittest(){
     if [ $? != 0 ]; then
         exit 1
     fi
-    find "./ppdet/modeling" -name 'tests' -type d -print0 | \
+    find "./ppdet" -name 'tests' -type d -print0 | \
         xargs -0 -I{} -n1 bash -c \
         'python -m unittest discover -v -s {}'
 }
@@ -20,9 +20,24 @@ unittest(){
 trap 'abort' 0
 set -e
 
-# install travis python dependencies
+# install travis python dependencies exclude pycocotools
 if [ -f ".travis/requirements.txt" ]; then
     pip install -r .travis/requirements.txt
+fi
+
+# install pycocotools
+if [ -n `pip list | grep pycocotools` ]; then
+  # install git if needed
+  if [ -n  `which git` ]; then
+    apt-get update
+    apt-get install -y git
+  fi;
+  git clone https://github.com/cocodataset/cocoapi.git
+  cd cocoapi/PythonAPI
+  make install
+  python setup.py install --user
+  cd ../..
+  rm -rf cocoapi
 fi
 
 export PYTHONPATH=`pwd`:$PYTHONPATH
