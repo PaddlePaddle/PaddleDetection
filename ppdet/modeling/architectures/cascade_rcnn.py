a0dbec77798a0f4bca55177cf91977ecc6c5fd57
Yang Zhang <yangzhang@live.com>
2020-02-13 15:23:37 +0800

    Clean up pyreader vestiges (#228)

--- ppdet/modeling/architectures/cascade_rcnn.py
+++ ppdet/modeling/architectures/cascade_rcnn.py
@@ -312,7 +312,7 @@ class CascadeRCNN(object):
             fields += ms_fields
             self.im_info_names = ['image', 'im_info'] + ms_fields
 
-        feed_vars = OrderedDict([(key, fluid.layers.data(
+        feed_vars = OrderedDict([(key, fluid.data(
             name=key,
             shape=inputs_def[key]['shape'],
             dtype=inputs_def[key]['dtype'],
