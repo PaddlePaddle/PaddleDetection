91823e97bb3e2cc451c9cfaa90ea3a134c58a05e
dengkaipeng <dengkaipeng@baidu.com>
2020-05-11 06:42:03 +0000

    disable daemon for thread

--- ppdet/data/parallel_map.py
+++ ppdet/data/parallel_map.py
@@ -119,7 +119,7 @@ class ParallelMap(object):
         self._producer = threading.Thread(
             target=self._produce,
             args=('producer-' + id, self._source, self._inq))
-        self._producer.daemon = True
+        self._producer.daemon = False
 
         self._consumers = []
         self._consumer_endsig = {}
@@ -130,7 +130,7 @@ class ParallelMap(object):
                 target=self._consume,
                 args=(consumer_id, self._inq, self._outq, self._worker))
             self._consumers.append(p)
-            p.daemon = True
+            p.daemon = use_process
             setattr(p, 'id', consumer_id)
             if use_process:
                 worker_set.add(p)
