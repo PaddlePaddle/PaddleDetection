81697cd042d21d608d564f41ef16205657ce9197
channings <chen_lingchi@163.com>
2020-06-03 14:30:14 +0800

    fix cpp bug cannot load video (#845)

--- deploy/cpp/scripts/bootstrap.sh
+++ deploy/cpp/scripts/bootstrap.sh
@@ -1,4 +1,12 @@
 # download pre-compiled opencv lib
+#OPENCV_URL=https://bj.bcebos.com/paddleseg/deploy/opencv3.4.6gcc4.8ffmpeg.tar.gz2
+#if [ ! -d "./deps/opencv3.4.6gcc4.8ffmpeg/" ]; then
+#    mkdir -p deps
+#    cd deps
+#    wget -c ${OPENCV_URL}
+#    tar xvfj opencv3.4.6gcc4.8ffmpeg.tar.gz2
+#    cd ..
+#fi
 OPENCV_URL=https://paddleseg.bj.bcebos.com/deploy/docker/opencv3gcc4.8.tar.bz2
 if [ ! -d "./deps/opencv3gcc4.8" ]; then
     mkdir -p deps
