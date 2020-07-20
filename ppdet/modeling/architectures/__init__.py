b3fb54a514b37ab78d9ede2f72c7bc94ac633091
sunxl1988 <935448535@qq.com>
2020-07-20 05:20:15 +0000

    test=master add htc model

--- ppdet/modeling/architectures/__init__.py
+++ ppdet/modeling/architectures/__init__.py
@@ -28,6 +28,7 @@ from . import faceboxes
 from . import fcos
 from . import cornernet_squeeze
 from . import ttfnet
+from . import htc
 
 from .faster_rcnn import *
 from .mask_rcnn import *
@@ -43,3 +44,4 @@ from .faceboxes import *
 from .fcos import *
 from .cornernet_squeeze import *
 from .ttfnet import *
+from .htc import *
