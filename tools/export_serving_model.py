dd1ea4305675a9b26b0effc1e7577e65e79d455e
still-wait <742925032@qq.com>
2020-08-27 05:13:10 +0000

    fix python path in export_serving_model.py

--- tools/export_serving_model.py
+++ tools/export_serving_model.py
@@ -16,7 +16,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import os
+import os, sys
+# add python path of PadleDetection to sys.path
+parent_path = os.path.abspath(os.path.join(__file__, *(['..'] * 2)))
+if parent_path not in sys.path:
+    sys.path.append(parent_path)
 
 from paddle import fluid
 
