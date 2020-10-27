48342c4df52e1477142ab2d4dbe9d0c626ef99d8
liuhui29 <liuhui29@baidu.com>
2020-10-27 21:06:30 +0800

    update sample code, call op in ppdet

--- ppdet/modeling/ops.py
+++ ppdet/modeling/ops.py
@@ -621,13 +621,12 @@ def yolo_box(
     .. code-block:: python
 
         import paddle
-        from paddle.fluid import core
         
         paddle.enable_static()
         x = paddle.static.data(name='x', shape=[None, 255, 13, 13], dtype='float32')
         img_size = paddle.static.data(name='img_size',shape=[None, 2],dtype='int64')
         anchors = [10, 13, 16, 30, 33, 23]
-        boxes,scores = core.ops.yolo_box(x=x, img_size=img_size, class_num=80, anchors=anchors,
+        boxes,scores = ops.yolo_box(x=x, img_size=img_size, class_num=80, anchors=anchors,
                                         conf_thresh=0.01, downsample_ratio=32)
     """
     helper = LayerHelper('yolo_box', **locals())
