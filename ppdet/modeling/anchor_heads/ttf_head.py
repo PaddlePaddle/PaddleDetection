e55a79ccc3ae16261a28a6a2a81f8083a5fb8c05
wangguanzhong <jerrywgz@126.com>
2020-11-19 11:28:49 +0800

    update_ttfnet_release0.5 (#1716)

--- ppdet/modeling/anchor_heads/ttf_head.py
+++ ppdet/modeling/anchor_heads/ttf_head.py
@@ -348,8 +348,7 @@ class TTFHead(object):
         return pred, target, weight
 
     def get_loss(self, pred_hm, pred_wh, target_hm, box_target, target_weight):
-        pred_hm = paddle.tensor.clamp(
-            fluid.layers.sigmoid(pred_hm), 1e-4, 1 - 1e-4)
+        pred_hm = paddle.clip(fluid.layers.sigmoid(pred_hm), 1e-4, 1 - 1e-4)
         hm_loss = self.ct_focal_loss(pred_hm, target_hm) * self.hm_weight
         shape = fluid.layers.shape(target_hm)
         shape.stop_gradient = True
