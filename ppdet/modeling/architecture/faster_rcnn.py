d594b8f3e1d38d097db6fe2ca420a7d49e0a31f2
Your Name <you@example.com>
2020-11-27 03:12:39 +0000

    fix merge

--- ppdet/modeling/architecture/faster_rcnn.py
+++ ppdet/modeling/architecture/faster_rcnn.py
@@ -36,34 +36,21 @@ class FasterRCNN(BaseArch):
 
     def model_arch(self):
         # Backbone
-<<<<<<< HEAD
-        body_feats = self.backbone(self.inputs)
-
-        spatial_scale = None
-=======
         body_feats = self.backbone(self.inputs, False)
         spatial_scale = [0.25, 0.125, 0.0625, 0.03125]
->>>>>>> 55de7f1178fe806386501fc1b24407f116622cfd
 
         # Neck
         if self.neck is not None:
             body_feats, spatial_scale = self.neck(body_feats)
-<<<<<<< HEAD
-=======
             rpn_feat, self.rpn_head_out = self.rpn_head(body_feats)
         else:
             rpn_feat, self.rpn_head_out = self.rpn_head([body_feats[-1]])    
->>>>>>> 55de7f1178fe806386501fc1b24407f116622cfd
 
         # RPN
         # rpn_head returns two list: rpn_feat, rpn_head_out
         # each element in rpn_feats contains rpn feature on each level,
         # and the length is 1 when the neck is not applied.
         # each element in rpn_head_out contains (rpn_rois_score, rpn_rois_delta)
-<<<<<<< HEAD
-        rpn_feat, self.rpn_head_out = self.rpn_head(self.inputs, body_feats)
-=======
->>>>>>> 55de7f1178fe806386501fc1b24407f116622cfd
 
         # Anchor
         # anchor_out returns a list,
@@ -74,17 +61,6 @@ class FasterRCNN(BaseArch):
         # compute targets here when training
         rois = self.proposal(self.inputs, self.rpn_head_out, self.anchor_out)
         # BBox Head
-<<<<<<< HEAD
-        bbox_feat, self.bbox_head_out = self.bbox_head(body_feats, rois,
-                                                       spatial_scale)
-
-        if self.inputs['mode'] == 'infer':
-            bbox_pred, bboxes = self.bbox_head.get_prediction(
-                self.bbox_head_out, rois)
-            # Refine bbox by the output from bbox_head at test stage
-            self.bboxes = self.bbox_post_process(bbox_pred, bboxes,
-                                                 self.inputs['im_info'])
-=======
         bbox_head_return_stage = 0
         if not self.bbox_head.use_resnetc5:
             bbox_feat = self.bbox_head.bbox_feat(body_feats, rois, spatial_scale, stage=bbox_head_return_stage)
@@ -106,7 +82,6 @@ class FasterRCNN(BaseArch):
                                                  self.inputs['im_shape'],
                                                  self.inputs['scale_factor'])
 
->>>>>>> 55de7f1178fe806386501fc1b24407f116622cfd
         else:
             # Proposal RoI for Mask branch
             # bboxes update at training stage only
@@ -125,21 +100,11 @@ class FasterRCNN(BaseArch):
         bbox_targets = self.proposal.get_targets()
         loss_bbox = self.bbox_head.get_loss(self.bbox_head_out, bbox_targets)
         loss.update(loss_bbox)
-<<<<<<< HEAD
-
-        total_loss = paddle.add_n(list(loss.values()))
-        loss.update({'loss': total_loss})
-        return loss
-
-    def get_pred(self, ):
-=======
-
         total_loss = paddle.add_n(list(loss.values()))
         loss.update({'loss': total_loss})
         return loss
 
     def get_pred(self, return_numpy=True):
->>>>>>> 55de7f1178fe806386501fc1b24407f116622cfd
         bbox, bbox_num = self.bboxes
         output = {
             'bbox': bbox.numpy(),
