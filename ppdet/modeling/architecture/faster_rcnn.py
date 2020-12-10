26bf8416b4339b204160f9b2c51ab08811c4988c
cnn <liuhui29@baidu.com>
2020-12-10 10:06:57 +0800

    [dygraph] add faster_rcnn_r50_fpn and faster_rcnn_r50 (#1843)
    * add faster_rcnn_r50_fpn and faster_rcnn_r50
    
    * update TestReader in dygraph
    
    * if gt_ploy is empty, continue
    
    * fix typo
    
    * judge gt_ploy is empty or not
    
    * fix judge condition
    
    * fix gt_ploy judge

--- ppdet/modeling/architecture/faster_rcnn.py
+++ ppdet/modeling/architecture/faster_rcnn.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from paddle import fluid
+import paddle
 from ppdet.core.workspace import register
 from .meta_arch import BaseArch
 
@@ -13,66 +13,91 @@ __all__ = ['FasterRCNN']
 class FasterRCNN(BaseArch):
     __category__ = 'architecture'
     __inject__ = [
-        'anchor',
-        'proposal',
-        'backbone',
-        'rpn_head',
-        'bbox_head',
+        'anchor', 'proposal', 'backbone', 'neck', 'rpn_head', 'bbox_head',
+        'bbox_post_process'
     ]
 
-    def __init__(self, anchor, proposal, backbone, rpn_head, bbox_head, *args,
-                 **kwargs):
-        super(FasterRCNN, self).__init__(*args, **kwargs)
+    def __init__(self,
+                 anchor,
+                 proposal,
+                 backbone,
+                 rpn_head,
+                 bbox_head,
+                 bbox_post_process,
+                 neck=None):
+        super(FasterRCNN, self).__init__()
         self.anchor = anchor
         self.proposal = proposal
         self.backbone = backbone
         self.rpn_head = rpn_head
         self.bbox_head = bbox_head
+        self.bbox_post_process = bbox_post_process
+        self.neck = neck
 
-    def model_arch(self, ):
+    def model_arch(self):
         # Backbone
-        bb_out = self.backbone(self.gbd)
-        self.gbd.update(bb_out)
+        body_feats = self.backbone(self.inputs)
+        spatial_scale = 0.0625
+
+        # Neck
+        if self.neck is not None:
+            body_feats, spatial_scale = self.neck(body_feats)
 
         # RPN
-        rpn_head_out = self.rpn_head(self.gbd)
-        self.gbd.update(rpn_head_out)
+        # rpn_head returns two list: rpn_feat, rpn_head_out
+        # each element in rpn_feats contains rpn feature on each level,
+        # and the length is 1 when the neck is not applied.
+        # each element in rpn_head_out contains (rpn_rois_score, rpn_rois_delta)
+        rpn_feat, self.rpn_head_out = self.rpn_head(self.inputs, body_feats)
 
         # Anchor
-        anchor_out = self.anchor(self.gbd)
-        self.gbd.update(anchor_out)
+        # anchor_out returns a list,
+        # each element contains (anchor, anchor_var)
+        self.anchor_out = self.anchor(rpn_feat)
 
-        # Proposal BBox
-        self.gbd['stage'] = 0
-        proposal_out = self.proposal(self.gbd)
-        self.gbd.update({'proposal_0': proposal_out})
-
+        # Proposal RoI
+        # compute targets here when training
+        rois = self.proposal(self.inputs, self.rpn_head_out, self.anchor_out)
         # BBox Head
-        bboxhead_out = self.bbox_head(self.gbd)
-        self.gbd.update({'bbox_head_0': bboxhead_out})
+        bbox_feat, self.bbox_head_out, self.bbox_head_feat_func = self.bbox_head(
+            body_feats, rois, spatial_scale)
 
-        if self.gbd['mode'] == 'infer':
-            bbox_out = self.proposal.post_process(self.gbd)
-            self.gbd.update(bbox_out)
+        if self.inputs['mode'] == 'infer':
+            bbox_pred, bboxes = self.bbox_head.get_prediction(
+                self.bbox_head_out, rois)
+            # Refine bbox by the output from bbox_head at test stage
+            self.bboxes = self.bbox_post_process(bbox_pred, bboxes,
+                                                 self.inputs['im_shape'],
+                                                 self.inputs['scale_factor'])
+
+        else:
+            # Proposal RoI for Mask branch
+            # bboxes update at training stage only
+            bbox_targets = self.proposal.get_targets()[0]
 
     def get_loss(self, ):
-        rpn_cls_loss, rpn_reg_loss = self.rpn_head.get_loss(self.gbd)
-        bbox_cls_loss, bbox_reg_loss = self.bbox_head.get_loss(self.gbd)
-        losses = [rpn_cls_loss, rpn_reg_loss, bbox_cls_loss, bbox_reg_loss]
-        loss = fluid.layers.sum(losses)
-        out = {
-            'loss': loss,
-            'loss_rpn_cls': rpn_cls_loss,
-            'loss_rpn_reg': rpn_reg_loss,
-            'loss_bbox_cls': bbox_cls_loss,
-            'loss_bbox_reg': bbox_reg_loss,
-        }
-        return out
+        loss = {}
 
-    def get_pred(self, ):
-        outs = {
-            "bbox": self.gbd['predicted_bbox'].numpy(),
-            "bbox_nums": self.gbd['predicted_bbox_nums'].numpy(),
-            'im_id': self.gbd['im_id'].numpy()
+        # RPN loss
+        rpn_loss_inputs = self.anchor.generate_loss_inputs(
+            self.inputs, self.rpn_head_out, self.anchor_out)
+        loss_rpn = self.rpn_head.get_loss(rpn_loss_inputs)
+        loss.update(loss_rpn)
+
+        # BBox loss
+        bbox_targets = self.proposal.get_targets()
+        loss_bbox = self.bbox_head.get_loss(self.bbox_head_out, bbox_targets)
+        loss.update(loss_bbox)
+        total_loss = paddle.add_n(list(loss.values()))
+        loss.update({'loss': total_loss})
+        return loss
+
+    def get_pred(self, return_numpy=True):
+        bbox, bbox_num = self.bboxes
+        output = {
+            'bbox': bbox.numpy(),
+            'bbox_num': bbox_num.numpy(),
+            'im_id': self.inputs['im_id'].numpy()
         }
-        return outs
+
+        return output
