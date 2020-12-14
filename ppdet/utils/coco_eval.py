fe13e2f110d9113d581e8232bd10168323391492
Saturn Lee <470454467@qq.com>
2020-12-14 22:05:34 +0800

    Update coco_eval.py
    all the subsequent bbox of current image  will be ignored incorrectly if the conditional in the line 314 met

--- ppdet/utils/coco_eval.py
+++ ppdet/utils/coco_eval.py
@@ -311,7 +311,9 @@ def bbox2out(results, clsid2catid, is_bbox_normalized=False):
             for j in range(num):
                 dt = bboxes[k]
                 clsid, score, xmin, ymin, xmax, ymax = dt.tolist()
-                if clsid < 0: continue
+                if clsid < 0: 
+                    k += 1
+                    continue
                 catid = (clsid2catid[int(clsid)])
 
                 if is_bbox_normalized:
