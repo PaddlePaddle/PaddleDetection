2f5d3c021c8880db71f01e888df71f7e352f8fb6
jerrywgz <jerrywgz@126.com>
2020-12-21 11:24:55 +0000

    remove fluid

--- dygraph/ppdet/modeling/architectures/ssd.py
+++ dygraph/ppdet/modeling/architectures/ssd.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from paddle import fluid
 from ppdet.core.workspace import register
 from .meta_arch import BaseArch
 
