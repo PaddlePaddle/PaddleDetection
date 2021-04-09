ef61961f91e257a38cf7171dc7048c8ffc532202
will-jl944 <lujiafeng@baidu.com>
2021-04-09 15:39:20 +0800

    In Pad, if size is not assigned by user, use normal division instead of floor division to calculate target height

--- ppdet/data/transform/operators.py
+++ ppdet/data/transform/operators.py
@@ -1862,7 +1862,7 @@ class Pad(BaseOperator):
                 im_h < h and im_w < w
             ), '(h, w) of target size should be greater than (im_h, im_w)'
         else:
-            h = np.ceil(im_h // self.size_divisor) * self.size_divisor
+            h = np.ceil(im_h / self.size_divisor) * self.size_divisor
             w = np.ceil(im_w / self.size_divisor) * self.size_divisor
 
         if h == im_h and w == im_w:
