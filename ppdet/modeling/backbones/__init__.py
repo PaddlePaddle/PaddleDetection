f8578d5b244a5032a34a4d2cf7d34236123202ce
ghostxsl <451323469@qq.com>
2021-05-13 17:12:31 +0800

    add res2net, test=develop

--- ppdet/modeling/backbones/__init__.py
+++ ppdet/modeling/backbones/__init__.py
@@ -21,6 +21,7 @@ from . import hrnet
 from . import blazenet
 from . import ghostnet
 from . import senet
+from . import res2net
 
 from .vgg import *
 from .resnet import *
@@ -31,3 +32,4 @@ from .hrnet import *
 from .blazenet import *
 from .ghostnet import *
 from .senet import *
+from .res2net import *
