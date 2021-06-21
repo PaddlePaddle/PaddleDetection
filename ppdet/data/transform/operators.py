9a157a14a5aa2b904260cd445259eade1700236a
lyuwenyu <wenyu.lyu@gmail.com>
2021-06-21 14:16:48 +0800

    update

--- ppdet/data/transform/operators.py
+++ ppdet/data/transform/operators.py
@@ -20,8 +20,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-from numpy.core.fromnumeric import size
-
 try:
     from collections.abc import Sequence
 except Exception:
