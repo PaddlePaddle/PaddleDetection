1843a090144fdaccb58c0d40c157cdaf4a2d21fe
lyuwenyu <wenyu.lyu@gmail.com>
2021-07-08 10:33:11 +0800

    fix import problem of _convert_attention_mask

--- ppdet/modeling/layers.py
+++ ppdet/modeling/layers.py
@@ -31,8 +31,6 @@ from . import ops
 from .initializer import xavier_uniform_, constant_
 
 from paddle.vision.ops import DeformConv2D
-from paddle.nn.layer import transformer
-_convert_attention_mask = transformer._convert_attention_mask
 
 
 def _to_list(l):
@@ -1193,6 +1191,27 @@ class Concat(nn.Layer):
 
     def extra_repr(self):
         return 'dim={}'.format(self.dim)
+
+
+def _convert_attention_mask(attn_mask, dtype):
+    """
+    Convert the attention mask to the target dtype we expect.
+    Parameters:
+        attn_mask (Tensor, optional): A tensor used in multi-head attention
+                to prevents attention to some unwanted positions, usually the
+                paddings or the subsequent positions. It is a tensor with shape
+                broadcasted to `[batch_size, n_head, sequence_length, sequence_length]`.
+                When the data type is bool, the unwanted positions have `False` 
+                values and the others have `True` values. When the data type is 
+                int, the unwanted positions have 0 values and the others have 1 
+                values. When the data type is float, the unwanted positions have 
+                `-INF` values and the others have 0 values. It can be None when 
+                nothing wanted or needed to be prevented attention to. Default None.
+        dtype (VarType): The target type of `attn_mask` we expect.
+    Returns:
+        Tensor: A Tensor with shape same as input `attn_mask`, with data type `dtype`.
+    """
+    return nn.layer.transformer._convert_attention_mask(attn_mask, dtype)
 
 
 class MultiHeadAttention(nn.Layer):
