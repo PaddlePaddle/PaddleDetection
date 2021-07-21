a831884acdc158b0ec23f6a305f08fb54372b562
zhiboniu <zhiboniu@163.com>
2021-07-21 13:03:30 +0000

    add keypoint top-down cpp infer; total:5ms,pre:0.7ms,infer:3.8ms,post:0.01ms

--- deploy/cpp/src/main.cc
+++ deploy/cpp/src/main.cc
@@ -32,18 +32,23 @@
 #endif
 
 #include "include/object_detector.h"
+#include "include/keypoint_detector.h"
+#include "include/preprocess_op.h"
 #include <gflags/gflags.h>
 
 
-DEFINE_string(model_dir, "", "Path of inference model");
+DEFINE_string(model_dir_det, "", "Path of object detector inference model");
+DEFINE_string(model_dir_keypoint, "", "Path of keypoint detector inference model");
 DEFINE_string(image_file, "", "Path of input image");
 DEFINE_string(image_dir, "", "Dir of input image, `image_file` has a higher priority.");
-DEFINE_int32(batch_size, 1, "batch_size");
+DEFINE_int32(batch_size_det, 1, "batch_size of object detector");
+DEFINE_int32(batch_size_keypoint, 1, "batch_size of keypoint detector");
 DEFINE_string(video_file, "", "Path of input video, `video_file` or `camera_id` has a highest priority.");
 DEFINE_int32(camera_id, -1, "Device id of camera to predict");
 DEFINE_bool(use_gpu, false, "Deprecated, please use `--device` to set the device you want to run.");
 DEFINE_string(device, "CPU", "Choose the device you want to run, it can be: CPU/GPU/XPU, default is CPU.");
-DEFINE_double(threshold, 0.5, "Threshold of score.");
+DEFINE_double(threshold_det, 0.5, "Threshold of score.");
+DEFINE_double(threshold_keypoint, 0.5, "Threshold of score.");
 DEFINE_string(output_dir, "output", "Directory of output visualization files.");
 DEFINE_string(run_mode, "fluid", "Mode of running(fluid/trt_fp32/trt_fp16/trt_int8)");
 DEFINE_int32(gpu_id, 0, "Device id of GPU to execute");
@@ -54,6 +59,7 @@ DEFINE_int32(trt_min_shape, 1, "Min shape of TRT DynamicShapeI");
 DEFINE_int32(trt_max_shape, 1280, "Max shape of TRT DynamicShapeI");
 DEFINE_int32(trt_opt_shape, 640, "Opt shape of TRT DynamicShapeI");
 DEFINE_bool(trt_calib_mode, false, "If the model is produced by TRT offline quantitative calibration, trt_calib_mode need to set True");
+
 
 void PrintBenchmarkLog(std::vector<double> det_time, int img_num){
   LOG(INFO) << "----------------------- Config info -----------------------";
@@ -72,18 +78,21 @@ void PrintBenchmarkLog(std::vector<double> det_time, int img_num){
   LOG(INFO) << "enable_mkldnn: " << (FLAGS_use_mkldnn ? "True" : "False");
   LOG(INFO) << "cpu_math_library_num_threads: " << FLAGS_cpu_threads;
   LOG(INFO) << "----------------------- Data info -----------------------";
-  LOG(INFO) << "batch_size: " << FLAGS_batch_size;
+  LOG(INFO) << "batch_size_det: " << FLAGS_batch_size_det;
+  LOG(INFO) << "batch_size_keypoint: " << FLAGS_batch_size_keypoint;
   LOG(INFO) << "input_shape: " << "dynamic shape";
   LOG(INFO) << "----------------------- Model info -----------------------";
-  FLAGS_model_dir.erase(FLAGS_model_dir.find_last_not_of("/") + 1);
-  LOG(INFO) << "model_name: " << FLAGS_model_dir.substr(FLAGS_model_dir.find_last_of('/') + 1);
+  FLAGS_model_dir_det.erase(FLAGS_model_dir_det.find_last_not_of("/") + 1);
+  LOG(INFO) << "model_name: " << FLAGS_model_dir_det.substr(FLAGS_model_dir_det.find_last_of('/') + 1);
+  FLAGS_model_dir_keypoint.erase(FLAGS_model_dir_keypoint.find_last_not_of("/") + 1);
+  LOG(INFO) << "model_name: " << FLAGS_model_dir_keypoint.substr(FLAGS_model_dir_keypoint.find_last_of('/') + 1);
   LOG(INFO) << "----------------------- Perf info ------------------------";
   LOG(INFO) << "Total number of predicted data: " << img_num
             << " and total time spent(ms): "
             << std::accumulate(det_time.begin(), det_time.end(), 0);
   LOG(INFO) << "preproce_time(ms): " << det_time[0] / img_num
             << ", inference_time(ms): " << det_time[1] / img_num
-            << ", postprocess_time(ms): " << det_time[2];
+            << ", postprocess_time(ms): " << det_time[2] / img_num;
 }
 
 static std::string DirName(const std::string &filepath) {
@@ -128,7 +137,8 @@ static void MkDirs(const std::string& path) {
 }
 
 void PredictVideo(const std::string& video_path,
-                  PaddleDetection::ObjectDetector* det) {
+                  PaddleDetection::ObjectDetector* det,
+                  PaddleDetection::KeyPointDetector* keypoint) {
   // Open video
   cv::VideoCapture capture;
   if (FLAGS_camera_id != -1){
@@ -164,6 +174,14 @@ void PredictVideo(const std::string& video_path,
   std::vector<double> det_times;
   auto labels = det->GetLabelList();
   auto colormap = PaddleDetection::GenerateColorMap(labels.size());
+
+
+  // Store keypoint results
+  std::vector<PaddleDetection::KeyPointResult> result_kpts;
+  std::vector<cv::Mat> imgs_kpts;
+  std::vector<std::vector<float>> center_bs;
+  std::vector<std::vector<float>> scale_bs;
+  std::vector<int> colormap_kpts = PaddleDetection::GenerateColorMap(20);
   // Capture all frames and do inference
   cv::Mat frame;
   int frame_id = 0;
@@ -199,12 +217,44 @@ void PredictVideo(const std::string& video_path,
           item.rect[2],
           item.rect[3]);
       }
-   }
+    }
 
-   cv::Mat out_im = PaddleDetection::VisualizeResult(
-        frame, result, labels, colormap, is_rbox);
+    if(keypoint)
+    {
+      int imsize = result.size();
+      for (int i=0; i<imsize; i++){
+        auto item = result[i];
+        cv::Mat crop_img;
+        std::vector<double> keypoint_times;
+        std::vector<int> rect = {item.rect[0], item.rect[1], item.rect[2], item.rect[3]};
+        std::vector<float> center;
+        std::vector<float> scale;
+        if(item.class_id == 0)
+        {
+          PaddleDetection::CropImg(frame, crop_img, rect, center, scale);
+          center_bs.emplace_back(center);
+          scale_bs.emplace_back(scale);
+          imgs_kpts.emplace_back(crop_img);
+        }
 
-    video_out.write(out_im);
+        if (imgs_kpts.size()==FLAGS_batch_size_keypoint || ((i==imsize-1)&&!imgs_kpts.empty()))
+        {
+          keypoint->Predict(imgs_kpts, center_bs, scale_bs, 0.5, 0, 1, &result_kpts, &keypoint_times);
+          imgs_kpts.clear();
+          center_bs.clear();
+          scale_bs.clear();
+        }
+      }
+      cv::Mat out_im = VisualizeKptsResult(frame, result_kpts, colormap_kpts);
+      video_out.write(out_im);
+    }
+    else{
+      // Visualization result
+      cv::Mat out_im = PaddleDetection::VisualizeResult(
+          frame, result, labels, colormap, is_rbox);
+      video_out.write(out_im);
+    }
+
     frame_id += 1;
   }
   capture.release();
@@ -212,23 +262,26 @@ void PredictVideo(const std::string& video_path,
 }
 
 void PredictImage(const std::vector<std::string> all_img_paths,
-                  const int batch_size,
-                  const double threshold,
+                  const int batch_size_det,
+                  const double threshold_det,
                   const bool run_benchmark,
                   PaddleDetection::ObjectDetector* det,
+                  PaddleDetection::KeyPointDetector* keypoint,
                   const std::string& output_dir = "output") {
   std::vector<double> det_t = {0, 0, 0};
-  int steps = ceil(float(all_img_paths.size()) / batch_size);
-  printf("total images = %d, batch_size = %d, total steps = %d\n",
-                all_img_paths.size(), batch_size, steps);
+  int steps = ceil(float(all_img_paths.size()) / batch_size_det);
+  int kpts_imgs = 0;
+  std::vector<double> keypoint_t = {0, 0, 0};
+  printf("total images = %d, batch_size_det = %d, total steps = %d\n",
+                all_img_paths.size(), batch_size_det, steps);
   for (int idx = 0; idx < steps; idx++) {
     std::vector<cv::Mat> batch_imgs;
-    int left_image_cnt = all_img_paths.size() - idx * batch_size;
-    if (left_image_cnt > batch_size) {
-      left_image_cnt = batch_size;
+    int left_image_cnt = all_img_paths.size() - idx * batch_size_det;
+    if (left_image_cnt > batch_size_det) {
+      left_image_cnt = batch_size_det;
     }
     for (int bs = 0; bs < left_image_cnt; bs++) {
-      std::string image_file_path = all_img_paths.at(idx * batch_size+bs);
+      std::string image_file_path = all_img_paths.at(idx * batch_size_det+bs);
       cv::Mat im = cv::imread(image_file_path, 1);
       batch_imgs.insert(batch_imgs.end(), im);
     }
@@ -237,83 +290,134 @@ void PredictImage(const std::vector<std::string> all_img_paths,
     std::vector<PaddleDetection::ObjectResult> result;
     std::vector<int> bbox_num;
     std::vector<double> det_times;
+
+    // Store keypoint results
+    std::vector<PaddleDetection::KeyPointResult> result_kpts;
+    std::vector<cv::Mat> imgs_kpts;
+    std::vector<std::vector<float>> center_bs;
+    std::vector<std::vector<float>> scale_bs;
+    std::vector<int> colormap_kpts = PaddleDetection::GenerateColorMap(20);
+
     bool is_rbox = false;
     if (run_benchmark) {
-      det->Predict(batch_imgs, threshold, 10, 10, &result, &bbox_num,  &det_times);
+      det->Predict(batch_imgs, threshold_det, 10, 10, &result, &bbox_num,  &det_times);
     } else {
-      det->Predict(batch_imgs, 0.5, 0, 1, &result, &bbox_num, &det_times);
-      // get labels and colormap
-      auto labels = det->GetLabelList();
-      auto colormap = PaddleDetection::GenerateColorMap(labels.size());
+      det->Predict(batch_imgs, 0.5, 10, 10, &result, &bbox_num, &det_times);
+    }
+    // get labels and colormap
+    auto labels = det->GetLabelList();
+    auto colormap = PaddleDetection::GenerateColorMap(labels.size());
+    int item_start_idx = 0;
+    for (int i = 0; i < left_image_cnt; i++) {
+      cv::Mat im = batch_imgs[i];
+      std::vector<PaddleDetection::ObjectResult> im_result;
+      int detect_num = 0;
+      for (int j = 0; j < bbox_num[i]; j++) {
+        PaddleDetection::ObjectResult item = result[item_start_idx + j];
+        if (item.confidence < threshold_det || item.class_id == -1) {
+          continue;
+        }
+        detect_num += 1;
+        im_result.push_back(item);
+        if (item.rect.size() > 6){
+          is_rbox = true;
+          printf("class=%d confidence=%.4f rect=[%d %d %d %d %d %d %d %d]\n",
+            item.class_id,
+            item.confidence,
+            item.rect[0],
+            item.rect[1],
+            item.rect[2],
+            item.rect[3],
+            item.rect[4],
+            item.rect[5],
+            item.rect[6],
+            item.rect[7]);
+        }
+        else{
+          printf("class=%d confidence=%.4f rect=[%d %d %d %d]\n",
+            item.class_id,
+            item.confidence,
+            item.rect[0],
+            item.rect[1],
+            item.rect[2],
+            item.rect[3]);
+        }
+      }
+      std::cout << all_img_paths.at(idx * batch_size_det + i) << " The number of detected box: " << detect_num << std::endl;
+      item_start_idx = item_start_idx + bbox_num[i];   
+      
+      std::vector<int> compression_params;
+      compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
+      compression_params.push_back(95);
+      std::string output_path(output_dir);
+      if (output_dir.rfind(OS_PATH_SEP) != output_dir.size() - 1) {
+        output_path += OS_PATH_SEP;
+      }
+      std::string image_file_path = all_img_paths.at(idx * batch_size_det + i);
+      if(keypoint)
+      {
+        int imsize = im_result.size();
+        for (int i=0; i<imsize; i++){
+          auto item = im_result[i];
+          cv::Mat crop_img;
+          std::vector<double> keypoint_times;
+          std::vector<int> rect = {item.rect[0], item.rect[1], item.rect[2], item.rect[3]};
+          std::vector<float> center;
+          std::vector<float> scale;
+          if(item.class_id == 0)
+          {
+            PaddleDetection::CropImg(im, crop_img, rect, center, scale);
+            center_bs.emplace_back(center);
+            scale_bs.emplace_back(scale);
+            imgs_kpts.emplace_back(crop_img);
+            kpts_imgs += 1;
+          }
 
-      int item_start_idx = 0;
-      for (int i = 0; i < left_image_cnt; i++) {
-        cv::Mat im = batch_imgs[i];
-        std::vector<PaddleDetection::ObjectResult> im_result;
-        int detect_num = 0;
- 
-        for (int j = 0; j < bbox_num[i]; j++) {
-          PaddleDetection::ObjectResult item = result[item_start_idx + j];
-          if (item.confidence < threshold || item.class_id == -1) {
-            continue;
-          }
-          detect_num += 1;
-          im_result.push_back(item);
-          if (item.rect.size() > 6){
-            is_rbox = true;
-            printf("class=%d confidence=%.4f rect=[%d %d %d %d %d %d %d %d]\n",
-              item.class_id,
-              item.confidence,
-              item.rect[0],
-              item.rect[1],
-              item.rect[2],
-              item.rect[3],
-              item.rect[4],
-              item.rect[5],
-              item.rect[6],
-              item.rect[7]);
-          }
-          else{
-            printf("class=%d confidence=%.4f rect=[%d %d %d %d]\n",
-              item.class_id,
-              item.confidence,
-              item.rect[0],
-              item.rect[1],
-              item.rect[2],
-              item.rect[3]);
+          if (imgs_kpts.size()==FLAGS_batch_size_keypoint || ((i==imsize-1)&&!imgs_kpts.empty()))
+          {
+            if (run_benchmark) {
+              keypoint->Predict(imgs_kpts, center_bs, scale_bs, 0.5, 10, 10, &result_kpts, &keypoint_times);
+            }
+            else{
+              keypoint->Predict(imgs_kpts, center_bs, scale_bs, 0.5, 0, 1, &result_kpts, &keypoint_times);
+            }
+            imgs_kpts.clear();
+            center_bs.clear();
+            scale_bs.clear();
+            keypoint_t[0] += keypoint_times[0];
+            keypoint_t[1] += keypoint_times[1];
+            keypoint_t[2] += keypoint_times[2];
           }
         }
-        std::cout << all_img_paths.at(idx * batch_size + i) << " The number of detected box: " << detect_num << std::endl;
-        item_start_idx = item_start_idx + bbox_num[i];
+        std::string kpts_savepath = output_path + "keypoint_" + image_file_path.substr(image_file_path.find_last_of('/') + 1);
+        cv::Mat kpts_vis_img = VisualizeKptsResult(im, result_kpts, colormap_kpts);
+        cv::imwrite(kpts_savepath, kpts_vis_img, compression_params);
+        printf("Visualized output saved as %s\n", kpts_savepath.c_str());
+      }
+      else{
         // Visualization result
         cv::Mat vis_img = PaddleDetection::VisualizeResult(
             im, im_result, labels, colormap, is_rbox);
-        std::vector<int> compression_params;
-        compression_params.push_back(CV_IMWRITE_JPEG_QUALITY);
-        compression_params.push_back(95);
-        std::string output_path(output_dir);
-        if (output_dir.rfind(OS_PATH_SEP) != output_dir.size() - 1) {
-          output_path += OS_PATH_SEP;
-        }
-        std::string image_file_path = all_img_paths.at(idx * batch_size + i);
-        output_path += image_file_path.substr(image_file_path.find_last_of('/') + 1);
-        cv::imwrite(output_path, vis_img, compression_params);
-        printf("Visualized output saved as %s\n", output_path.c_str());       
+        std::string det_savepath = output_path + image_file_path.substr(image_file_path.find_last_of('/') + 1);
+        cv::imwrite(det_savepath, vis_img, compression_params);
+        printf("Visualized output saved as %s\n", det_savepath.c_str());   
       }
     }
+    
     det_t[0] += det_times[0];
     det_t[1] += det_times[1];
     det_t[2] += det_times[2];
   }
   PrintBenchmarkLog(det_t, all_img_paths.size());
+  PrintBenchmarkLog(keypoint_t, kpts_imgs);
 }
 
 int main(int argc, char** argv) {
   // Parsing command-line
   google::ParseCommandLineFlags(&argc, &argv, true);
-  if (FLAGS_model_dir.empty()
+  if (FLAGS_model_dir_det.empty()
       || (FLAGS_image_file.empty() && FLAGS_image_dir.empty() && FLAGS_video_file.empty())) {
-    std::cout << "Usage: ./main --model_dir=/PATH/TO/INFERENCE_MODEL/ "
+    std::cout << "Usage: ./main --model_dir_det=/PATH/TO/INFERENCE_MODEL/ (--model_dir_keypoint=/PATH/TO/INFERENCE_MODEL/)"
                 << "--image_file=/PATH/TO/INPUT/IMAGE/" << std::endl;
     return -1;
   }
@@ -332,13 +436,22 @@ int main(int argc, char** argv) {
     return -1;
   }
   // Load model and create a object detector
-  PaddleDetection::ObjectDetector det(FLAGS_model_dir, FLAGS_device, FLAGS_use_mkldnn,
-                        FLAGS_cpu_threads, FLAGS_run_mode, FLAGS_batch_size,FLAGS_gpu_id,
+  PaddleDetection::ObjectDetector det(FLAGS_model_dir_det, FLAGS_device, FLAGS_use_mkldnn,
+                        FLAGS_cpu_threads, FLAGS_run_mode, FLAGS_batch_size_det,FLAGS_gpu_id,
                         FLAGS_trt_min_shape, FLAGS_trt_max_shape, FLAGS_trt_opt_shape,
 			FLAGS_trt_calib_mode);
+
+  PaddleDetection::KeyPointDetector* keypoint = nullptr;
+  if (!FLAGS_model_dir_keypoint.empty())
+  {
+      keypoint = new PaddleDetection::KeyPointDetector(FLAGS_model_dir_keypoint, FLAGS_device, FLAGS_use_mkldnn,
+                        FLAGS_cpu_threads, FLAGS_run_mode, FLAGS_batch_size_det,FLAGS_gpu_id,
+                        FLAGS_trt_min_shape, FLAGS_trt_max_shape, FLAGS_trt_opt_shape,
+			FLAGS_trt_calib_mode);
+  }
   // Do inference on input video or image
   if (!FLAGS_video_file.empty() || FLAGS_camera_id != -1) {
-    PredictVideo(FLAGS_video_file, &det);
+    PredictVideo(FLAGS_video_file, &det, keypoint);
   } else if (!FLAGS_image_file.empty() || !FLAGS_image_dir.empty()) {
     if (!PathExists(FLAGS_output_dir)) {
       MkDirs(FLAGS_output_dir);
@@ -347,8 +460,8 @@ int main(int argc, char** argv) {
     std::vector<cv::String> cv_all_img_paths;
     if (!FLAGS_image_file.empty()) {
       all_img_paths.push_back(FLAGS_image_file);
-      if (FLAGS_batch_size > 1) {
-        std::cout << "batch_size should be 1, when set `image_file`." << std::endl;
+      if (FLAGS_batch_size_det > 1) {
+        std::cout << "batch_size_det should be 1, when set `image_file`." << std::endl;
 	return -1;
       }
     } else {
@@ -357,8 +470,10 @@ int main(int argc, char** argv) {
           all_img_paths.push_back(img_path);
         }
     }
-    PredictImage(all_img_paths, FLAGS_batch_size, FLAGS_threshold,
-		 FLAGS_run_benchmark, &det, FLAGS_output_dir);
+    PredictImage(all_img_paths, FLAGS_batch_size_det, FLAGS_threshold_det,
+		 FLAGS_run_benchmark, &det, keypoint, FLAGS_output_dir);
   }
+  delete keypoint;
+  keypoint = nullptr;
   return 0;
 }
