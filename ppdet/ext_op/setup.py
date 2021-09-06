14d86b8e9ea4a5783441e97815ada9aa7ca8d0a4
wangxinxin08 <wangxinxin08@baidu.com>
2021-09-06 06:39:29 +0000

    polish the code of rbox iou

--- ppdet/ext_op/setup.py
+++ ppdet/ext_op/setup.py
@@ -1,6 +1,14 @@
-from paddle.utils.cpp_extension import CUDAExtension, setup
+import paddle
+from paddle.utils.cpp_extension import CppExtension, CUDAExtension, setup
 
 if __name__ == "__main__":
-    setup(
-        name='rbox_iou_ops',
-        ext_modules=CUDAExtension(sources=['rbox_iou_op.cc', 'rbox_iou_op.cu']))
+    if paddle.device.is_compiled_with_cuda():
+        setup(
+            name='rbox_iou_ops',
+            ext_modules=CUDAExtension(
+                sources=['rbox_iou_op.cc', 'rbox_iou_op.cu'],
+                extra_compile_args={'cxx': ['-DPADDLE_WITH_CUDA']}))
+    else:
+        setup(
+            name='rbox_iou_ops',
+            ext_modules=CppExtension(sources=['rbox_iou_op.cc']))
