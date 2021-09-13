df5225659fb217f62cc6cca4dd4095c9e9844cf5
shangliang Xu <ghostxsl@users.noreply.github.com>
2021-09-13 18:45:01 +0800

    [ce tests] Add trt mode and amp in ce test (#4174)
    * [ce tests] add trt_mode in ppyolo
    
    * [ce tests] set amp in tests.sh

--- tests/test.sh
+++ tests/test.sh
@@ -304,7 +304,11 @@ else
                     continue
                 fi
 
-                set_autocast=$(func_set_params "${autocast_key}" "${autocast}")
+                if [ ${autocast} = "amp" ]; then
+                    set_autocast="--fp16"
+                else
+                    set_autocast=$(func_set_params "${autocast_key}" "${autocast}")
+                fi
                 set_epoch=$(func_set_params "${epoch_key}" "${epoch_num}")
                 set_pretrain=$(func_set_params "${pretrain_model_key}" "${pretrain_model_value}")
                 set_batchsize=$(func_set_params "${train_batch_key}" "${train_batch_value}")
@@ -319,11 +323,11 @@ else
 
                 set_save_model=$(func_set_params "${save_model_key}" "${save_log}")
                 if [ ${#gpu} -le 2 ];then  # train with cpu or single gpu
-                    cmd="${python} ${run_train} ${set_use_gpu}  ${set_save_model} ${set_epoch} ${set_pretrain} ${set_autocast} ${set_batchsize} ${set_train_params1} "
+                    cmd="${python} ${run_train} ${set_use_gpu}  ${set_save_model} ${set_epoch} ${set_pretrain} ${set_batchsize} ${set_train_params1} ${set_autocast} "
                 elif [ ${#gpu} -le 15 ];then  # train with multi-gpu
-                    cmd="${python} -m paddle.distributed.launch --gpus=${gpu} ${run_train} ${set_save_model} ${set_epoch} ${set_pretrain} ${set_autocast} ${set_batchsize} ${set_train_params1}"
+                    cmd="${python} -m paddle.distributed.launch --gpus=${gpu} ${run_train} ${set_save_model} ${set_epoch} ${set_pretrain} ${set_batchsize} ${set_train_params1} ${set_autocast}"
                 else     # train with multi-machine
-                    cmd="${python} -m paddle.distributed.launch --ips=${ips} --gpus=${gpu} ${run_train} ${set_save_model} ${set_pretrain} ${set_epoch} ${set_autocast} ${set_batchsize} ${set_train_params1}"
+                    cmd="${python} -m paddle.distributed.launch --ips=${ips} --gpus=${gpu} ${run_train} ${set_save_model} ${set_pretrain} ${set_epoch} ${set_batchsize} ${set_train_params1} ${set_autocast}"
                 fi
                 # run train
                 eval "unset CUDA_VISIBLE_DEVICES"
