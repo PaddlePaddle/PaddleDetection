108c2a23df9180c5ba20a125d62dcfdb43c3cc11
nemonameless <nemonameless@qq.com>
2021-09-28 19:51:41 +0000

    add iou head

--- ppdet/modeling/heads/centernet_head.py
+++ ppdet/modeling/heads/centernet_head.py
@@ -18,7 +18,7 @@ import paddle.nn as nn
 import paddle.nn.functional as F
 from paddle.nn.initializer import KaimingUniform
 from ppdet.core.workspace import register
-from ppdet.modeling.losses import CTFocalLoss
+from ppdet.modeling.losses import CTFocalLoss, GIoULoss
 
 
 class ConvLayer(nn.Layer):
@@ -67,7 +67,9 @@ class CenterNetHead(nn.Layer):
         regress_ltrb (bool): whether to regress left/top/right/bottom or
             width/height for a box, true by default
         size_weight (float): the weight of box size loss, 0.1 by default.
+        size_loss (): the type of size regression loss, 'L1 loss' by default.
         offset_weight (float): the weight of center offset loss, 1 by default.
+        iou_weight (float): the weight of iou head loss, 0 by default.
 
     """
 
@@ -80,13 +82,18 @@ class CenterNetHead(nn.Layer):
                  heatmap_weight=1,
                  regress_ltrb=True,
                  size_weight=0.1,
-                 offset_weight=1):
+                 size_loss='L1',
+                 offset_weight=1,
+                 iou_weight=0):
         super(CenterNetHead, self).__init__()
         self.weights = {
             'heatmap': heatmap_weight,
             'size': size_weight,
-            'offset': offset_weight
+            'offset': offset_weight,
+            'iou': iou_weight
         }
+
+        # heatmap head
         self.heatmap = nn.Sequential(
             ConvLayer(
                 in_channels, head_planes, kernel_size=3, padding=1, bias=True),
@@ -100,6 +107,8 @@ class CenterNetHead(nn.Layer):
                 bias=True))
         with paddle.no_grad():
             self.heatmap[2].conv.bias[:] = -2.19
+
+        # size(ltrb or wh) head
         self.size = nn.Sequential(
             ConvLayer(
                 in_channels, head_planes, kernel_size=3, padding=1, bias=True),
@@ -111,13 +120,45 @@ class CenterNetHead(nn.Layer):
                 stride=1,
                 padding=0,
                 bias=True))
-        self.offset = nn.Sequential(
-            ConvLayer(
-                in_channels, head_planes, kernel_size=3, padding=1, bias=True),
-            nn.ReLU(),
-            ConvLayer(
-                head_planes, 2, kernel_size=1, stride=1, padding=0, bias=True))
-        self.focal_loss = CTFocalLoss()
+        self.size_loss = size_loss
+
+        # offset head (optinal)
+        self.offset_weight = offset_weight
+        if self.offset_weight > 0:
+            self.offset = nn.Sequential(
+                ConvLayer(
+                    in_channels,
+                    head_planes,
+                    kernel_size=3,
+                    padding=1,
+                    bias=True),
+                nn.ReLU(),
+                ConvLayer(
+                    head_planes,
+                    2,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0,
+                    bias=True))
+
+        # iou head (optinal)
+        self.iou_weight = iou_weight
+        if self.iou_weight > 0:
+            self.iou = nn.Sequential(
+                ConvLayer(
+                    in_channels,
+                    head_planes,
+                    kernel_size=3,
+                    padding=1,
+                    bias=True),
+                nn.ReLU(),
+                ConvLayer(
+                    head_planes,
+                    4 if regress_ltrb else 2,
+                    kernel_size=1,
+                    stride=1,
+                    padding=0,
+                    bias=True))
 
     @classmethod
     def from_config(cls, cfg, input_shape):
@@ -128,23 +169,31 @@ class CenterNetHead(nn.Layer):
     def forward(self, feat, inputs):
         heatmap = self.heatmap(feat)
         size = self.size(feat)
-        offset = self.offset(feat)
+        offset = self.offset(feat) if self.offset_weight else None
+        iou = self.iou(feat) if self.iou_weight else None
+
         if self.training:
-            loss = self.get_loss(heatmap, size, offset, self.weights, inputs)
+            loss = self.get_loss(
+                inputs, self.weights, heatmap, size, offset=offset, iou=iou)
             return loss
         else:
             heatmap = F.sigmoid(heatmap)
-            return {'heatmap': heatmap, 'size': size, 'offset': offset}
+            head_outs = {'heatmap': heatmap, 'size': size}
+            if self.offset_weight:
+                head_outs.update({'offset': offset})
+            if self.iou_weight:
+                head_outs.update({'iou': iou})
+            return head_outs
 
-    def get_loss(self, heatmap, size, offset, weights, inputs):
+    def get_loss(self, inputs, weights, heatmap, size, offset=None, iou=None):
+        # heatmap head loss: CTFocalLoss
         heatmap_target = inputs['heatmap']
-        size_target = inputs['size']
-        offset_target = inputs['offset']
+        heatmap = paddle.clip(F.sigmoid(heatmap), 1e-4, 1 - 1e-4)
+        heatmap_loss = CTFocalLoss(heatmap, heatmap_target)
+
+        # size head loss: L1 loss or GIoU loss
         index = inputs['index']
         mask = inputs['index_mask']
-        heatmap = paddle.clip(F.sigmoid(heatmap), 1e-4, 1 - 1e-4)
-        heatmap_loss = self.focal_loss(heatmap, heatmap_target)
-
         size = paddle.transpose(size, perm=[0, 2, 3, 1])
         size_n, size_h, size_w, size_c = size.shape
         size = paddle.reshape(size, shape=[size_n, -1, size_c])
@@ -162,32 +211,86 @@ class CenterNetHead(nn.Layer):
         size_mask = paddle.cast(size_mask, dtype=pos_size.dtype)
         pos_num = size_mask.sum()
         size_mask.stop_gradient = True
-        size_target.stop_gradient = True
-        size_loss = F.l1_loss(
-            pos_size * size_mask, size_target * size_mask, reduction='sum')
-        size_loss = size_loss / (pos_num + 1e-4)
+        if self.size_loss == 'L1':
+            size_target = inputs['size']
+            size_target.stop_gradient = True
+            size_loss = F.l1_loss(
+                pos_size * size_mask, size_target * size_mask, reduction='sum')
+            size_loss = size_loss / (pos_num + 1e-4)
+        elif self.size_loss == 'giou':
+            size_target = inputs['bbox_xys']
+            size_target.stop_gradient = True
+            centers_x = (size_target[:, :, 0:1] + size_target[:, :, 2:3]) / 2.0
+            centers_y = (size_target[:, :, 1:2] + size_target[:, :, 3:4]) / 2.0
+            x1 = centers_x - pos_size[:, :, 0:1]
+            y1 = centers_y - pos_size[:, :, 1:2]
+            x2 = centers_x + pos_size[:, :, 2:3]
+            y2 = centers_y + pos_size[:, :, 3:4]
+            pred_boxes = paddle.concat([x1, y1, x2, y2], axis=-1)
+            giou_loss = GIoULoss(reduction='sum')
+            size_loss = giou_loss(
+                pred_boxes * size_mask,
+                size_target * size_mask,
+                iou_weight=size_mask,
+                loc_reweight=None)
+            size_loss = size_loss / (pos_num + 1e-4)
 
-        offset = paddle.transpose(offset, perm=[0, 2, 3, 1])
-        offset_n, offset_h, offset_w, offset_c = offset.shape
-        offset = paddle.reshape(offset, shape=[offset_n, -1, offset_c])
-        pos_offset = paddle.gather_nd(offset, index=index)
-        offset_mask = paddle.expand_as(mask, pos_offset)
-        offset_mask = paddle.cast(offset_mask, dtype=pos_offset.dtype)
-        pos_num = offset_mask.sum()
-        offset_mask.stop_gradient = True
-        offset_target.stop_gradient = True
-        offset_loss = F.l1_loss(
-            pos_offset * offset_mask,
-            offset_target * offset_mask,
-            reduction='sum')
-        offset_loss = offset_loss / (pos_num + 1e-4)
+        # offset head loss: L1 loss
+        if offset and self.offset_weight:
+            offset_target = inputs['offset']
+            offset = paddle.transpose(offset, perm=[0, 2, 3, 1])
+            offset_n, offset_h, offset_w, offset_c = offset.shape
+            offset = paddle.reshape(offset, shape=[offset_n, -1, offset_c])
+            pos_offset = paddle.gather_nd(offset, index=index)
+            offset_mask = paddle.expand_as(mask, pos_offset)
+            offset_mask = paddle.cast(offset_mask, dtype=pos_offset.dtype)
+            pos_num = offset_mask.sum()
+            offset_mask.stop_gradient = True
+            offset_target.stop_gradient = True
+            offset_loss = F.l1_loss(
+                pos_offset * offset_mask,
+                offset_target * offset_mask,
+                reduction='sum')
+            offset_loss = offset_loss / (pos_num + 1e-4)
 
-        det_loss = weights['heatmap'] * heatmap_loss + weights[
-            'size'] * size_loss + weights['offset'] * offset_loss
+        # iou head loss: GIoU loss
+        if iou and self.iou_weight:
+            iou = paddle.transpose(iou, perm=[0, 2, 3, 1])
+            iou_n, iou_h, iou_w, iou_c = iou.shape
+            iou = paddle.reshape(iou, shape=[iou_n, -1, iou_c])
+            pos_iou = paddle.gather_nd(iou, index=index)
+            iou_mask = paddle.expand_as(mask, pos_iou)
+            iou_mask = paddle.cast(iou_mask, dtype=pos_iou.dtype)
+            pos_num = iou_mask.sum()
+            iou_mask.stop_gradient = True
+            gt_bbox_xys = inputs['bbox_xys']
+            gt_bbox_xys.stop_gradient = True
+            centers_x = (gt_bbox_xys[:, :, 0:1] + gt_bbox_xys[:, :, 2:3]) / 2.0
+            centers_y = (gt_bbox_xys[:, :, 1:2] + gt_bbox_xys[:, :, 3:4]) / 2.0
+            x1 = centers_x - pos_size[:, :, 0:1]
+            y1 = centers_y - pos_size[:, :, 1:2]
+            x2 = centers_x + pos_size[:, :, 2:3]
+            y2 = centers_y + pos_size[:, :, 3:4]
+            pred_boxes = paddle.concat([x1, y1, x2, y2], axis=-1)
+            giou_loss = GIoULoss(reduction='sum')
+            iou_loss = giou_loss(
+                pred_boxes * iou_mask,
+                gt_bbox_xys * iou_mask,
+                iou_weight=iou_mask,
+                loc_reweight=None)
+            iou_loss = iou_loss / (pos_num + 1e-4)
 
-        return {
-            'det_loss': det_loss,
+        losses = {
             'heatmap_loss': heatmap_loss,
             'size_loss': size_loss,
-            'offset_loss': offset_loss
         }
+        det_loss = weights['heatmap'] * heatmap_loss + weights[
+            'size'] * size_loss
+        if self.offset_weight:
+            losses.update({'offset_loss': offset_loss})
+            det_loss = det_loss + weights['offset'] * offset_loss
+        if self.iou_weight:
+            losses.update({'iou_loss': iou_loss})
+            det_loss = det_loss + weights['iou'] * iou_loss
+        losses.update({'det_loss': det_loss})
+        return losses
