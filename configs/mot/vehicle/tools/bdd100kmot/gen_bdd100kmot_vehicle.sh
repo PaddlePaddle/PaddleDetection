13c1fef14b26cbde838607b5100d12014e7febba
Feng Ni <nemonameless@qq.com>
2021-09-30 13:36:19 +0800

    [MOT] add MOT feature models (#4105)
    * add pedestrian vehicle cfgs
    
    * fix metric category
    
    * fix mot metric
    
    * fix mot metric
    
    * update pedestrian and vehicle modelzoo
    
    * update headtracking modelzoo
    
    * add tools for data processing
    
    * add data pre-process code
    
    * fix tools code format

---
+++ configs/mot/vehicle/tools/bdd100kmot/gen_bdd100kmot_vehicle.sh
@@ -0,0 +1,16 @@
+data_path=bdd100k_path
+img_dir=${data_path}/images
+label_dir=${data_path}/labels
+save_path=${data_path}/bdd100kmot_vehicle
+
+phasetrain=train
+phaseval=val
+classes=2,3,4,9,10
+
+# gen mot dataste
+python bdd100k2mot.py --data_path=${data_path}  --phase=${phasetrain} --classes=${classes} --img_dir=${img_dir} --label_dir=${label_dir} --save_path=${save_path}
+python bdd100k2mot.py --data_path=${data_path} --phase=${phaseval} --classes=${classes} --img_dir=${img_dir} --label_dir=${label_dir} --save_path=${save_path}
+
+# gen new labels_with_ids
+python gen_labels_MOT.py --mot_data=${data_path} --phase=${phasetrain}
+python gen_labels_MOT.py --mot_data=${data_path} --phase=${phaseval}
