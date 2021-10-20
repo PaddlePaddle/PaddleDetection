78c1ec3ebd5e3866eb183690aabf22b5647d7bbe
jiweimaster <2413914266@qq.com>
2021-10-20 04:57:37 +0000

    edit copyright

--- deploy/third_engine/demo_mnn/python/demo_mnn.py
+++ deploy/third_engine/demo_mnn/python/demo_mnn.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# reference from https://github.com/RangiLyu/nanodet
+# reference from https://github.com/RangiLyu/nanodet/tree/main/demo_mnn
 
 # -*- coding: utf-8 -*-
 import argparse
