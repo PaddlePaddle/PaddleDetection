973b907c2ac7dfbf7138e83473db69217b08fb72
wangguanzhong <jerrywgz@126.com>
2021-11-13 11:51:56 +0800

    add draw_center_traj (#4567)
    * add draw_center_traj

--- deploy/pptracking/python/mot_jde_infer.py
+++ deploy/pptracking/python/mot_jde_infer.py
@@ -220,6 +220,11 @@ def predict_video(detector, camera_id):
     num_classes = detector.num_classes
     data_type = 'mcmot' if num_classes > 1 else 'mot'
     ids2names = detector.pred_config.labels
+    center_traj = None
+    entrance = None
+    records = None
+    if FLAGS.draw_center_traj:
+        center_traj = {}
 
     if num_classes == 1:
         id_set = set()
@@ -231,6 +236,7 @@ def predict_video(detector, camera_id):
         entrance = [0, height / 2., width, height / 2.]
 
     video_fps = fps
+
     while (1):
         ret, frame = capture.read()
         if not ret:
@@ -260,10 +266,9 @@ def predict_video(detector, camera_id):
             prev_center = statistic['prev_center']
             records = statistic['records']
 
-        elif num_classes > 1 and do_entrance_counting:
+        elif num_classes > 1 and FLAGS.do_entrance_counting:
             raise NotImplementedError(
                 'Multi-class flow counting is not implemented now!')
-
         im = plot_tracking_dict(
             frame,
             num_classes,
@@ -274,7 +279,9 @@ def predict_video(detector, camera_id):
             fps=fps,
             ids2names=ids2names,
             do_entrance_counting=FLAGS.do_entrance_counting,
-            entrance=entrance)
+            entrance=entrance,
+            records=records,
+            center_traj=center_traj)
 
         if FLAGS.save_images:
             save_dir = os.path.join(FLAGS.output_dir, video_name.split('.')[-2])
