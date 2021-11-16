8a96743527c1b913aabd2a5107d4c1b6cba5c3cb
mmglove <gmm_email@163.com>
2021-11-16 14:16:52 +0800

    update benchmark case

--- benchmark/prepare.sh
+++ benchmark/prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-pip3.7 install -U pip Cython
-pip3.7 install -r requirements.txt
+pip install -U pip Cython
+pip install -r requirements.txt
 
 mv ./dataset/coco/download_coco.py . && rm -rf ./dataset/coco/* && mv ./download_coco.py ./dataset/coco/
 # prepare lite train data
@@ -9,8 +9,9 @@ wget -nc -P ./dataset/coco/ https://paddledet.bj.bcebos.com/data/coco_benchmark.
 cd ./dataset/coco/ && tar -xvf coco_benchmark.tar && mv -u coco_benchmark/* .
 rm -rf coco_benchmark/
 
+cd ../
 rm -rf ./dataset/mot/*
 # prepare mot mini train data
 wget -nc -P ./dataset/mot/ https://paddledet.bj.bcebos.com/data/mot_benchmark.tar
 cd ./dataset/mot/ && tar -xvf mot_benchmark.tar && mv -u mot_benchmark/* .
-rm -rf mot_benchmark/
+#rm -rf mot_benchmark/
