e4504e8dc6e8a45013df79e61c857533d086d51a
lyuwenyu <wenyu.lyu@gmail.com>
2022-04-02 11:05:49 +0000

    modified:   README.md 	modified:   README_en.md 	deleted:    ../../deploy/third_engine/demo_openvino/python/demo/000000570688.jpg 	modified:   ../../deploy/third_engine/demo_openvino/python/openvino_benchmark.py 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_l_320_processed.onnx 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_l_416_processed.onnx 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_l_640_processed.onnx 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_m_320_processed.onnx 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_m_416_processed.onnx 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_s_320_processed.onnx 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_s_416_processed.onnx 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_xs_320_processed.onnx 	deleted:    ../../deploy/third_engine/demo_openvino/python/out_onnxsim/picodet_xs_416_processed.onnx

--- deploy/third_engine/demo_openvino/python/openvino_benchmark.py
+++ deploy/third_engine/demo_openvino/python/openvino_benchmark.py
@@ -1,3 +1,17 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import cv2
 import numpy as np
 import time
@@ -48,7 +62,7 @@ def benchmark(img_file,onnx_file,re_shape):
 
     time_avg = timeall/loop_num
 
-    print(f'inference_time(ms): min={round(time_min*1000, 2)}, max = {round(time_max*1000, 2)}, avg = {round(time_avg*1000, 2)}')
+    print(f'inference_time(ms): min={round(time_min*1000, 2)}, max = {round(time_max*1000, 1)}, avg = {round(time_avg*1000, 1)}')
 
 if __name__=='__main__':
 
@@ -62,5 +76,4 @@ if __name__=='__main__':
     
     
     args = parser.parse_args()
-    
     benchmark(args.img_path, args.onnx_path,args.in_shape)
