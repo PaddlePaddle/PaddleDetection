d26c65c1e82e4e2cc3793253070328c9bbf3af0d
wangxinxin08 <wangxinxin08@baidu.com>
2022-04-06 07:39:10 +0000

    add check version code

--- ppdet/modeling/layers.py
+++ ppdet/modeling/layers.py
@@ -473,11 +473,15 @@ class MultiClassNMS(object):
             kwargs.update({'rois_num': bbox_num})
         if background_label > -1:
             kwargs.update({'background_label': background_label})
-        if self.trt:
-            kwargs.pop('trt')
+        kwargs.pop('trt')
+        # TODO(wangxinxin08): paddle version should be develop or 2.3 and above to run nms on tensorrt
+        if self.trt and (int(paddle.version.major) == 0 or
+                         (int(paddle.version.major) >= 2 and
+                          int(paddle.version.minor) >= 3)):
+            # TODO(wangxinxin08): tricky switch to run nms on tensorrt
             kwargs.update({'nms_eta': 1.1})
             bbox, bbox_num, _ = ops.multiclass_nms(bboxes, score, **kwargs)
-            mask = bbox[..., 0:1] != -1
+            mask = paddle.slice(bbox, [-1], [0], [1]) != -1
             bbox = paddle.masked_select(bbox, mask)
             return bbox, bbox_num, None
         else:
