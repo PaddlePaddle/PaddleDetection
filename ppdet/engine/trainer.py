ceefbd413a11031cd34c5d21743ed90c470b236a
jerrywgz <jerrywgz@126.com>
2022-05-10 05:48:47 +0000

    fix yaml constructor in eval & export

--- ppdet/engine/trainer.py
+++ ppdet/engine/trainer.py
@@ -63,18 +63,20 @@ class Trainer(object):
         self.is_loaded_weights = False
 
         # build data loader
+        cpaital_mode = self.mode.capitalize()
         if cfg.architecture in MOT_ARCH and self.mode in ['eval', 'test']:
-            self.dataset = create('{}MOTDataset'.format(self.mode.capitalize(
-            )))()
+            self.dataset = self.cfg['{}MOTDataset'.format(
+                cpaital_mode)] = create('{}MOTDataset'.format(cpaital_mode))()
         else:
-            self.dataset = create('{}Dataset'.format(self.mode.capitalize()))()
+            self.dataset = self.cfg['{}Dataset'.format(cpaital_mode)] = create(
+                '{}Dataset'.format(cpaital_mode))()
 
         if cfg.architecture == 'DeepSORT' and self.mode == 'train':
             logger.error('DeepSORT has no need of training on mot dataset.')
             sys.exit(1)
 
         if self.mode == 'train':
-            self.loader = create('{}Reader'.format(self.mode.capitalize()))(
+            self.loader = create('{}Reader'.format(cpaital_mode))(
                 self.dataset, cfg.worker_num)
 
         if cfg.architecture == 'JDE' and self.mode == 'train':
@@ -336,6 +338,8 @@ class Trainer(object):
     def train(self, validate=False):
         assert self.mode == 'train', "Model not in 'train' mode"
         Init_mark = False
+        if validate:
+            self.cfg.EvalDataset = create("EvalDataset")()
 
         model = self.model
         if self.cfg.get('fleet', False):
