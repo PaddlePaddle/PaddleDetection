d922544994f4115fb05f88d8e764289905be8716
lyuwenyu <wenyu.lyu@gmail.com>
2022-06-02 17:45:40 +0800

    cache, multi thread for coco dataset

--- ppdet/data/source/coco.py
+++ ppdet/data/source/coco.py
@@ -13,7 +13,10 @@
 # limitations under the License.
 
 import os
+import pickle
 import numpy as np
+import concurrent.futures as futures
+
 from ppdet.core.workspace import register, serializable
 from .dataset import DetDataset
 
@@ -49,7 +52,10 @@ class COCODataSet(DetDataset):
                  sample_num=-1,
                  load_crowd=False,
                  allow_empty=False,
-                 empty_ratio=1.):
+                 empty_ratio=1.,
+                 num_threads=32,
+                 cache_path='',
+                 force_reload=True):
         super(COCODataSet, self).__init__(dataset_dir, image_dir, anno_path,
                                           data_fields, sample_num)
         self.load_image_only = False
@@ -57,6 +63,9 @@ class COCODataSet(DetDataset):
         self.load_crowd = load_crowd
         self.allow_empty = allow_empty
         self.empty_ratio = empty_ratio
+        self.num_threads = num_threads
+        self.cache_path = cache_path
+        self.force_reload = force_reload
 
     def _sample_empty(self, records, num):
         # if empty_ratio is out of [0. ,1.), do not sample the records
@@ -70,7 +79,16 @@ class COCODataSet(DetDataset):
 
     def parse_dataset(self):
         anno_path = os.path.join(self.dataset_dir, self.anno_path)
-        image_dir = os.path.join(self.dataset_dir, self.image_dir)
+        cache_path = os.path.join(
+            self.dataset_dir,
+            self.cache_path) if self.cache_path else self.cache_path
+
+        if cache_path and os.path.exists(cache_path) and not self.force_reload:
+            blob = pickle.load(open(cache_path, 'rb'))
+            for k, v in blob.items():
+                setattr(self, k, v)
+
+            return
 
         assert anno_path.endswith('.json'), \
             'invalid coco annotation file: ' + anno_path
@@ -79,9 +97,6 @@ class COCODataSet(DetDataset):
         img_ids = coco.getImgIds()
         img_ids.sort()
         cat_ids = coco.getCatIds()
-        records = []
-        empty_records = []
-        ct = 0
 
         self.catid2clsid = dict({catid: i for i, catid in enumerate(cat_ids)})
         self.cname2cid = dict({
@@ -94,156 +109,171 @@ class COCODataSet(DetDataset):
             logger.warning('Annotation file: {} does not contains ground truth '
                            'and load image information only.'.format(anno_path))
 
-        for img_id in img_ids:
-            img_anno = coco.loadImgs([img_id])[0]
-            im_fname = img_anno['file_name']
-            im_w = float(img_anno['width'])
-            im_h = float(img_anno['height'])
+        with futures.ThreadPoolExecutor(self.num_threads) as executor:
+            _records = executor.map(self.parse_img, img_ids, coco)
+            _records = [data for data in _records if data is not None]
+            _records = _records[:self.
+                                sample_num] if self.sample_num > 0 else _records
 
-            im_path = os.path.join(image_dir,
-                                   im_fname) if image_dir else im_fname
-            is_empty = False
-            if not os.path.exists(im_path):
-                logger.warning('Illegal image file: {}, and it will be '
-                               'ignored'.format(im_path))
-                continue
+        assert len(_records) > 0, 'not found any coco record in %s' % (
+            anno_path)
 
-            if im_w < 0 or im_h < 0:
-                logger.warning('Illegal width: {} or height: {} in annotation, '
-                               'and im_id: {} will be ignored'.format(
-                                   im_w, im_h, img_id))
-                continue
+        records = [record for (record, is_empty) in _records if not is_empty]
+        empty_records = [record for (record, is_empty) in _records if is_empty]
 
-            coco_rec = {
-                'im_file': im_path,
-                'im_id': np.array([img_id]),
-                'h': im_h,
-                'w': im_w,
-            } if 'image' in self.data_fields else {}
-
-            if not self.load_image_only:
-                ins_anno_ids = coco.getAnnIds(
-                    imgIds=[img_id], iscrowd=None if self.load_crowd else False)
-                instances = coco.loadAnns(ins_anno_ids)
-
-                bboxes = []
-                is_rbox_anno = False
-                for inst in instances:
-                    # check gt bbox
-                    if inst.get('ignore', False):
-                        continue
-                    if 'bbox' not in inst.keys():
-                        continue
-                    else:
-                        if not any(np.array(inst['bbox'])):
-                            continue
-
-                    # read rbox anno or not
-                    is_rbox_anno = True if len(inst['bbox']) == 5 else False
-                    if is_rbox_anno:
-                        xc, yc, box_w, box_h, angle = inst['bbox']
-                        x1 = xc - box_w / 2.0
-                        y1 = yc - box_h / 2.0
-                        x2 = x1 + box_w
-                        y2 = y1 + box_h
-                    else:
-                        x1, y1, box_w, box_h = inst['bbox']
-                        x2 = x1 + box_w
-                        y2 = y1 + box_h
-                    eps = 1e-5
-                    if inst['area'] > 0 and x2 - x1 > eps and y2 - y1 > eps:
-                        inst['clean_bbox'] = [
-                            round(float(x), 3) for x in [x1, y1, x2, y2]
-                        ]
-                        if is_rbox_anno:
-                            inst['clean_rbox'] = [xc, yc, box_w, box_h, angle]
-                        bboxes.append(inst)
-                    else:
-                        logger.warning(
-                            'Found an invalid bbox in annotations: im_id: {}, '
-                            'area: {} x1: {}, y1: {}, x2: {}, y2: {}.'.format(
-                                img_id, float(inst['area']), x1, y1, x2, y2))
-
-                num_bbox = len(bboxes)
-                if num_bbox <= 0 and not self.allow_empty:
-                    continue
-                elif num_bbox <= 0:
-                    is_empty = True
-
-                gt_bbox = np.zeros((num_bbox, 4), dtype=np.float32)
-                if is_rbox_anno:
-                    gt_rbox = np.zeros((num_bbox, 5), dtype=np.float32)
-                gt_theta = np.zeros((num_bbox, 1), dtype=np.int32)
-                gt_class = np.zeros((num_bbox, 1), dtype=np.int32)
-                is_crowd = np.zeros((num_bbox, 1), dtype=np.int32)
-                gt_poly = [None] * num_bbox
-
-                has_segmentation = False
-                for i, box in enumerate(bboxes):
-                    catid = box['category_id']
-                    gt_class[i][0] = self.catid2clsid[catid]
-                    gt_bbox[i, :] = box['clean_bbox']
-                    # xc, yc, w, h, theta
-                    if is_rbox_anno:
-                        gt_rbox[i, :] = box['clean_rbox']
-                    is_crowd[i][0] = box['iscrowd']
-                    # check RLE format 
-                    if 'segmentation' in box and box['iscrowd'] == 1:
-                        gt_poly[i] = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
-                    elif 'segmentation' in box and box['segmentation']:
-                        if not np.array(box['segmentation']
-                                        ).size > 0 and not self.allow_empty:
-                            bboxes.pop(i)
-                            gt_poly.pop(i)
-                            np.delete(is_crowd, i)
-                            np.delete(gt_class, i)
-                            np.delete(gt_bbox, i)
-                        else:
-                            gt_poly[i] = box['segmentation']
-                        has_segmentation = True
-
-                if has_segmentation and not any(
-                        gt_poly) and not self.allow_empty:
-                    continue
-
-                if is_rbox_anno:
-                    gt_rec = {
-                        'is_crowd': is_crowd,
-                        'gt_class': gt_class,
-                        'gt_bbox': gt_bbox,
-                        'gt_rbox': gt_rbox,
-                        'gt_poly': gt_poly,
-                    }
-                else:
-                    gt_rec = {
-                        'is_crowd': is_crowd,
-                        'gt_class': gt_class,
-                        'gt_bbox': gt_bbox,
-                        'gt_poly': gt_poly,
-                    }
-
-                for k, v in gt_rec.items():
-                    if k in self.data_fields:
-                        coco_rec[k] = v
-
-                # TODO: remove load_semantic
-                if self.load_semantic and 'semantic' in self.data_fields:
-                    seg_path = os.path.join(self.dataset_dir, 'stuffthingmaps',
-                                            'train2017', im_fname[:-3] + 'png')
-                    coco_rec.update({'semantic': seg_path})
-
-            logger.debug('Load file: {}, im_id: {}, h: {}, w: {}.'.format(
-                im_path, img_id, im_h, im_w))
-            if is_empty:
-                empty_records.append(coco_rec)
-            else:
-                records.append(coco_rec)
-            ct += 1
-            if self.sample_num > 0 and ct >= self.sample_num:
-                break
-        assert ct > 0, 'not found any coco record in %s' % (anno_path)
-        logger.debug('{} samples in file {}'.format(ct, anno_path))
+        logger.debug('{} samples in file {}'.format(len(_records), anno_path))
         if self.allow_empty and len(empty_records) > 0:
             empty_records = self._sample_empty(empty_records, len(records))
             records += empty_records
         self.roidbs = records
+
+        if cache_path:
+            blob = {
+                'roidbs': self.roidbs,
+                'catid2clsid': self.catid2clsid,
+                'cname2cid': self.cname2cid
+            }
+            pickle.dump(blob, open(cache_path, 'wb'))
+
+    def parse_img(self, img_id, coco):
+        image_dir = os.path.join(self.dataset_dir, self.image_dir)
+        img_anno = coco.loadImgs([img_id])[0]
+        im_fname = img_anno['file_name']
+        im_w = float(img_anno['width'])
+        im_h = float(img_anno['height'])
+
+        im_path = os.path.join(image_dir, im_fname) if image_dir else im_fname
+        is_empty = False
+        if not os.path.exists(im_path):
+            logger.warning('Illegal image file: {}, and it will be '
+                           'ignored'.format(im_path))
+            return
+
+        if im_w < 0 or im_h < 0:
+            logger.warning('Illegal width: {} or height: {} in annotation, '
+                           'and im_id: {} will be ignored'.format(im_w, im_h,
+                                                                  img_id))
+            return
+
+        coco_rec = {
+            'im_file': im_path,
+            'im_id': np.array([img_id]),
+            'h': im_h,
+            'w': im_w,
+        } if 'image' in self.data_fields else {}
+
+        if not self.load_image_only:
+            ins_anno_ids = coco.getAnnIds(
+                imgIds=[img_id], iscrowd=None if self.load_crowd else False)
+            instances = coco.loadAnns(ins_anno_ids)
+
+            bboxes = []
+            is_rbox_anno = False
+            for inst in instances:
+                # check gt bbox
+                if inst.get('ignore', False):
+                    continue
+                if 'bbox' not in inst.keys():
+                    continue
+                else:
+                    if not any(np.array(inst['bbox'])):
+                        continue
+
+                # read rbox anno or not
+                is_rbox_anno = True if len(inst['bbox']) == 5 else False
+                if is_rbox_anno:
+                    xc, yc, box_w, box_h, angle = inst['bbox']
+                    x1 = xc - box_w / 2.0
+                    y1 = yc - box_h / 2.0
+                    x2 = x1 + box_w
+                    y2 = y1 + box_h
+                else:
+                    x1, y1, box_w, box_h = inst['bbox']
+                    x2 = x1 + box_w
+                    y2 = y1 + box_h
+                eps = 1e-5
+                if inst['area'] > 0 and x2 - x1 > eps and y2 - y1 > eps:
+                    inst['clean_bbox'] = [
+                        round(float(x), 3) for x in [x1, y1, x2, y2]
+                    ]
+                    if is_rbox_anno:
+                        inst['clean_rbox'] = [xc, yc, box_w, box_h, angle]
+                    bboxes.append(inst)
+                else:
+                    logger.warning(
+                        'Found an invalid bbox in annotations: im_id: {}, '
+                        'area: {} x1: {}, y1: {}, x2: {}, y2: {}.'.format(
+                            img_id, float(inst['area']), x1, y1, x2, y2))
+
+            num_bbox = len(bboxes)
+            if num_bbox <= 0 and not self.allow_empty:
+                return
+
+            elif num_bbox <= 0:
+                is_empty = True
+
+            gt_bbox = np.zeros((num_bbox, 4), dtype=np.float32)
+            if is_rbox_anno:
+                gt_rbox = np.zeros((num_bbox, 5), dtype=np.float32)
+            gt_theta = np.zeros((num_bbox, 1), dtype=np.int32)
+            gt_class = np.zeros((num_bbox, 1), dtype=np.int32)
+            is_crowd = np.zeros((num_bbox, 1), dtype=np.int32)
+            gt_poly = [None] * num_bbox
+
+            has_segmentation = False
+            for i, box in enumerate(bboxes):
+                catid = box['category_id']
+                gt_class[i][0] = self.catid2clsid[catid]
+                gt_bbox[i, :] = box['clean_bbox']
+                # xc, yc, w, h, theta
+                if is_rbox_anno:
+                    gt_rbox[i, :] = box['clean_rbox']
+                is_crowd[i][0] = box['iscrowd']
+                # check RLE format 
+                if 'segmentation' in box and box['iscrowd'] == 1:
+                    gt_poly[i] = [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0]]
+                elif 'segmentation' in box and box['segmentation']:
+                    if not np.array(box[
+                            'segmentation']).size > 0 and not self.allow_empty:
+                        bboxes.pop(i)
+                        gt_poly.pop(i)
+                        np.delete(is_crowd, i)
+                        np.delete(gt_class, i)
+                        np.delete(gt_bbox, i)
+                    else:
+                        gt_poly[i] = box['segmentation']
+                    has_segmentation = True
+
+            if has_segmentation and not any(gt_poly) and not self.allow_empty:
+                return
+
+            if is_rbox_anno:
+                gt_rec = {
+                    'is_crowd': is_crowd,
+                    'gt_class': gt_class,
+                    'gt_bbox': gt_bbox,
+                    'gt_rbox': gt_rbox,
+                    'gt_poly': gt_poly,
+                }
+            else:
+                gt_rec = {
+                    'is_crowd': is_crowd,
+                    'gt_class': gt_class,
+                    'gt_bbox': gt_bbox,
+                    'gt_poly': gt_poly,
+                }
+
+            for k, v in gt_rec.items():
+                if k in self.data_fields:
+                    coco_rec[k] = v
+
+            # TODO: remove load_semantic
+            if self.load_semantic and 'semantic' in self.data_fields:
+                seg_path = os.path.join(self.dataset_dir, 'stuffthingmaps',
+                                        'train2017', im_fname[:-3] + 'png')
+                coco_rec.update({'semantic': seg_path})
+
+        logger.debug('Load file: {}, im_id: {}, h: {}, w: {}.'.format(
+            im_path, img_id, im_h, im_w))
+
+        return (coco_rec, is_empty)
