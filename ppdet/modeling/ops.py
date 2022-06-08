23b4ab48380379096e35172356f0f5548d292e53
nemonameless <nemonameless@qq.com>
2022-06-08 07:21:26 +0000

    revert for iou_similarity collect_fpn_proposals

--- ppdet/modeling/ops.py
+++ ppdet/modeling/ops.py
@@ -121,6 +121,170 @@ def batch_norm(ch,
 
 
 @paddle.jit.not_to_static
+def iou_similarity(x, y, box_normalized=True, name=None):
+    """
+    Computes intersection-over-union (IOU) between two box lists.
+    Box list 'X' should be a LoDTensor and 'Y' is a common Tensor,
+    boxes in 'Y' are shared by all instance of the batched inputs of X.
+    Given two boxes A and B, the calculation of IOU is as follows:
+    $$
+    IOU(A, B) = 
+    \\frac{area(A\\cap B)}{area(A)+area(B)-area(A\\cap B)}
+    $$
+    Args:
+        x (Tensor): Box list X is a 2-D Tensor with shape [N, 4] holds N 
+             boxes, each box is represented as [xmin, ymin, xmax, ymax], 
+             the shape of X is [N, 4]. [xmin, ymin] is the left top 
+             coordinate of the box if the input is image feature map, they
+             are close to the origin of the coordinate system. 
+             [xmax, ymax] is the right bottom coordinate of the box.
+             The data type is float32 or float64.
+        y (Tensor): Box list Y holds M boxes, each box is represented as 
+             [xmin, ymin, xmax, ymax], the shape of X is [N, 4]. 
+             [xmin, ymin] is the left top coordinate of the box if the 
+             input is image feature map, and [xmax, ymax] is the right 
+             bottom coordinate of the box. The data type is float32 or float64.
+        box_normalized(bool): Whether treat the priorbox as a normalized box.
+            Set true by default.
+        name(str, optional): For detailed information, please refer 
+            to :ref:`api_guide_Name`. Usually name is no need to set and 
+            None by default. 
+    Returns:
+        Tensor: The output of iou_similarity op, a tensor with shape [N, M] 
+              representing pairwise iou scores. The data type is same with x.
+    Examples:
+        .. code-block:: python
+            import paddle
+            from ppdet.modeling import ops
+            paddle.enable_static()
+            x = paddle.static.data(name='x', shape=[None, 4], dtype='float32')
+            y = paddle.static.data(name='y', shape=[None, 4], dtype='float32')
+            iou = ops.iou_similarity(x=x, y=y)
+    """
+
+    if in_dygraph_mode():
+        out = core.ops.iou_similarity(x, y, 'box_normalized', box_normalized)
+        return out
+    else:
+        helper = LayerHelper("iou_similarity", **locals())
+        out = helper.create_variable_for_type_inference(dtype=x.dtype)
+
+        helper.append_op(
+            type="iou_similarity",
+            inputs={"X": x,
+                    "Y": y},
+            attrs={"box_normalized": box_normalized},
+            outputs={"Out": out})
+        return out
+
+
+@paddle.jit.not_to_static
+def collect_fpn_proposals(multi_rois,
+                          multi_scores,
+                          min_level,
+                          max_level,
+                          post_nms_top_n,
+                          rois_num_per_level=None,
+                          name=None):
+    """
+    
+    **This OP only supports LoDTensor as input**. Concat multi-level RoIs 
+    (Region of Interest) and select N RoIs with respect to multi_scores. 
+    This operation performs the following steps:
+    1. Choose num_level RoIs and scores as input: num_level = max_level - min_level
+    2. Concat multi-level RoIs and scores
+    3. Sort scores and select post_nms_top_n scores
+    4. Gather RoIs by selected indices from scores
+    5. Re-sort RoIs by corresponding batch_id
+    Args:
+        multi_rois(list): List of RoIs to collect. Element in list is 2-D 
+            LoDTensor with shape [N, 4] and data type is float32 or float64, 
+            N is the number of RoIs.
+        multi_scores(list): List of scores of RoIs to collect. Element in list 
+            is 2-D LoDTensor with shape [N, 1] and data type is float32 or
+            float64, N is the number of RoIs.
+        min_level(int): The lowest level of FPN layer to collect
+        max_level(int): The highest level of FPN layer to collect
+        post_nms_top_n(int): The number of selected RoIs
+        rois_num_per_level(list, optional): The List of RoIs' numbers. 
+            Each element is 1-D Tensor which contains the RoIs' number of each 
+            image on each level and the shape is [B] and data type is 
+            int32, B is the number of images. If it is not None then return 
+            a 1-D Tensor contains the output RoIs' number of each image and 
+            the shape is [B]. Default: None
+        name(str, optional): For detailed information, please refer 
+            to :ref:`api_guide_Name`. Usually name is no need to set and 
+            None by default.
+    Returns:
+        Variable:
+        fpn_rois(Variable): 2-D LoDTensor with shape [N, 4] and data type is 
+        float32 or float64. Selected RoIs. 
+        rois_num(Tensor): 1-D Tensor contains the RoIs's number of each 
+        image. The shape is [B] and data type is int32. B is the number of 
+        images. 
+    Examples:
+        .. code-block:: python
+           
+            import paddle
+            from ppdet.modeling import ops
+            paddle.enable_static()
+            multi_rois = []
+            multi_scores = []
+            for i in range(4):
+                multi_rois.append(paddle.static.data(
+                    name='roi_'+str(i), shape=[None, 4], dtype='float32', lod_level=1))
+            for i in range(4):
+                multi_scores.append(paddle.static.data(
+                    name='score_'+str(i), shape=[None, 1], dtype='float32', lod_level=1))
+            fpn_rois = ops.collect_fpn_proposals(
+                multi_rois=multi_rois, 
+                multi_scores=multi_scores,
+                min_level=2, 
+                max_level=5, 
+                post_nms_top_n=2000)
+    """
+    check_type(multi_rois, 'multi_rois', list, 'collect_fpn_proposals')
+    check_type(multi_scores, 'multi_scores', list, 'collect_fpn_proposals')
+    num_lvl = max_level - min_level + 1
+    input_rois = multi_rois[:num_lvl]
+    input_scores = multi_scores[:num_lvl]
+
+    if in_dygraph_mode():
+        assert rois_num_per_level is not None, "rois_num_per_level should not be None in dygraph mode."
+        attrs = ('post_nms_topN', post_nms_top_n)
+        output_rois, rois_num = core.ops.collect_fpn_proposals(
+            input_rois, input_scores, rois_num_per_level, *attrs)
+        return output_rois, rois_num
+
+    else:
+        helper = LayerHelper('collect_fpn_proposals', **locals())
+        dtype = helper.input_dtype('multi_rois')
+        check_dtype(dtype, 'multi_rois', ['float32', 'float64'],
+                    'collect_fpn_proposals')
+        output_rois = helper.create_variable_for_type_inference(dtype)
+        output_rois.stop_gradient = True
+
+        inputs = {
+            'MultiLevelRois': input_rois,
+            'MultiLevelScores': input_scores,
+        }
+        outputs = {'FpnRois': output_rois}
+        if rois_num_per_level is not None:
+            inputs['MultiLevelRoIsNum'] = rois_num_per_level
+            rois_num = helper.create_variable_for_type_inference(dtype='int32')
+            rois_num.stop_gradient = True
+            outputs['RoisNum'] = rois_num
+        else:
+            rois_num = None
+        helper.append_op(
+            type='collect_fpn_proposals',
+            inputs=inputs,
+            outputs=outputs,
+            attrs={'post_nms_topN': post_nms_top_n})
+        return output_rois, rois_num
+
+
+@paddle.jit.not_to_static
 def distribute_fpn_proposals(fpn_rois,
                              min_level,
                              max_level,
