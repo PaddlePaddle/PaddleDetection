fb739c59aa75050efa6f1afc38d9c10c2b8deab4
ghostxsl <451323469@qq.com>
2022-06-20 11:12:01 +0800

    [dev] add amp eval
    cast dtype in load_pretrain_weight

--- ppdet/engine/trainer.py
+++ ppdet/engine/trainer.py
@@ -65,6 +65,8 @@ class Trainer(object):
         self.mode = mode.lower()
         self.optimizer = None
         self.is_loaded_weights = False
+        self.use_amp = self.cfg.get('amp', False)
+        self.amp_level = self.cfg.get('amp_level', 'O1')
 
         # build data loader
         capital_mode = self.mode.capitalize()
@@ -124,17 +126,6 @@ class Trainer(object):
         else:
             self.model.load_meanstd(cfg['TestReader']['sample_transforms'])
 
-        self.use_ema = ('use_ema' in cfg and cfg['use_ema'])
-        if self.use_ema:
-            ema_decay = self.cfg.get('ema_decay', 0.9998)
-            cycle_epoch = self.cfg.get('cycle_epoch', -1)
-            ema_decay_type = self.cfg.get('ema_decay_type', 'threshold')
-            self.ema = ModelEMA(
-                self.model,
-                decay=ema_decay,
-                ema_decay_type=ema_decay_type,
-                cycle_epoch=cycle_epoch)
-
         # EvalDataset build with BatchSampler to evaluate in single device
         # TODO: multi-device evaluate
         if self.mode == 'eval':
@@ -161,6 +152,20 @@ class Trainer(object):
             if self.cfg.get('unstructured_prune'):
                 self.pruner = create('UnstructuredPruner')(self.model,
                                                            steps_per_epoch)
+
+        if self.use_amp and self.amp_level == 'O2':
+            self.model = paddle.amp.decorate(
+                models=self.model, level=self.amp_level)
+        self.use_ema = ('use_ema' in cfg and cfg['use_ema'])
+        if self.use_ema:
+            ema_decay = self.cfg.get('ema_decay', 0.9998)
+            cycle_epoch = self.cfg.get('cycle_epoch', -1)
+            ema_decay_type = self.cfg.get('ema_decay_type', 'threshold')
+            self.ema = ModelEMA(
+                self.model,
+                decay=ema_decay,
+                ema_decay_type=ema_decay_type,
+                cycle_epoch=cycle_epoch)
 
         self._nranks = dist.get_world_size()
         self._local_rank = dist.get_rank()
@@ -387,13 +392,10 @@ class Trainer(object):
             model = paddle.nn.SyncBatchNorm.convert_sync_batchnorm(model)
 
         # enabel auto mixed precision mode
-        use_amp = self.cfg.get('amp', False)
-        amp_level = self.cfg.get('amp_level', 'O1')
-        if use_amp:
+        if self.use_amp:
             scaler = paddle.amp.GradScaler(
                 enable=self.cfg.use_gpu or self.cfg.use_npu,
                 init_loss_scaling=self.cfg.get('init_loss_scaling', 1024))
-            model = paddle.amp.decorate(models=model, level=amp_level)
         # get distributed model
         if self.cfg.get('fleet', False):
             model = fleet.distributed_model(model)
@@ -438,9 +440,9 @@ class Trainer(object):
                 self._compose_callback.on_step_begin(self.status)
                 data['epoch_id'] = epoch_id
 
-                if use_amp:
+                if self.use_amp:
                     with paddle.amp.auto_cast(
-                            enable=self.cfg.use_gpu, level=amp_level):
+                            enable=self.cfg.use_gpu, level=self.amp_level):
                         # model forward
                         outputs = model(data)
                         loss = outputs['loss']
@@ -532,7 +534,12 @@ class Trainer(object):
             self.status['step_id'] = step_id
             self._compose_callback.on_step_begin(self.status)
             # forward
-            outs = self.model(data)
+            if self.use_amp:
+                with paddle.amp.auto_cast(
+                        enable=self.cfg.use_gpu, level=self.amp_level):
+                    outs = self.model(data)
+            else:
+                outs = self.model(data)
 
             # update metrics
             for metric in self._metrics:
