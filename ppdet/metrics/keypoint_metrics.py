579927439fc5c268c96eb6b1d5fb02c2574115a6
RachelXu7 <molixu7@gmail.com>
2022-06-20 02:29:46 +0000

    Improve the Usability of Keypoint Metric

--- ppdet/metrics/keypoint_metrics.py
+++ ppdet/metrics/keypoint_metrics.py
@@ -21,6 +21,7 @@ from pycocotools.cocoeval import COCOeval
 from ..modeling.keypoint_utils import oks_nms
 from scipy.io import loadmat, savemat
 from ppdet.utils.logger import setup_logger
+import paddle
 logger = setup_logger(__name__)
 
 __all__ = ['KeyPointTopDownCOCOEval', 'KeyPointTopDownMPIIEval']
