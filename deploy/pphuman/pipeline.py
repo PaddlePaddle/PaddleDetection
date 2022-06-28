742606a3415efaaa539286099b3bcc8cd3e1727b
zhiboniu <zhiboniu@163.com>
2022-06-28 08:35:16 +0000

    rename imshow name; test=document_fix

--- deploy/pphuman/pipeline.py
+++ deploy/pphuman/pipeline.py
@@ -618,7 +618,7 @@ class PipePredictor(object):
                                                   center_traj)  # visualize
                         writer.write(im)
                         if self.file_name is None:  # use camera_id
-                            cv2.imshow('PPHuman&&PPVehicle', im)
+                            cv2.imshow('Paddle-Pipeline', im)
                             if cv2.waitKey(1) & 0xFF == ord('q'):
                                 break
                     continue
@@ -771,7 +771,7 @@ class PipePredictor(object):
                                           center_traj)  # visualize
                 writer.write(im)
                 if self.file_name is None:  # use camera_id
-                    cv2.imshow('PPHuman', im)
+                    cv2.imshow('Paddle-Pipeline', im)
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
