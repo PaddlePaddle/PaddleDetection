c7b7010589167bab6e72e6c311d2619f93576748
XYZ-916 <1290573099@qq.com>
2022-08-15 11:17:12 +0000

    add docs for illegal parking

--- deploy/pipeline/download.py
+++ deploy/pipeline/download.py
@@ -32,6 +32,10 @@ WEIGHTS_HOME = osp.expanduser("~/.cache/paddle/infer_weights")
 MODEL_URL_MD5_DICT = {
     "https://paddleocr.bj.bcebos.com/PP-OCRv3/chinese/ch_PP-OCRv3_det_infer.tar":
     '982d2d8d83e55f5f981e96a7b941fff5',
+    'https://bj.bcebos.com/v1/paddledet/models/pipeline/ch_PP-OCRv3_det_infer.tar.gz':
+    '1b8eae0f098635699bd4e8bccf3067a7',
+    'https://bj.bcebos.com/v1/paddledet/models/pipeline/ch_PP-OCRv3_rec_infer.tar.gz':
+    '64fa0e0701efd93c7db52a9b685b3de6',
     "https://paddleocr.bj.bcebos.com/PP-OCRv3/chinese/ch_PP-OCRv3_rec_infer.tar":
     '5f021b88518bdeda2cb4a3aacc481024',
     "https://bj.bcebos.com/v1/paddledet/models/pipeline/mot_ppyoloe_l_36e_ppvehicle.zip":
@@ -133,7 +137,7 @@ def _download(url, path, md5sum=None):
             raise RuntimeError("Downloading from {} failed with code "
                                "{}!".format(url, req.status_code))
 
-        # For protecting download interupted, download to
+        # For protecting download interupted,9b685b3de6 download to
         # tmp_fullname firstly, move tmp_fullname to fullname
         # after download finished
         tmp_fullname = fullname + "_tmp"
