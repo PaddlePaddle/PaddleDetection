5a3ac4359569f7995e244c4155003dbaa67f12b5
pk_hk <82303451+pkhk-1@users.noreply.github.com>
2022-11-07 11:27:30 +0800

    [smalldet] add tal_cr and custompan with transfomer, add reg_max pred (#7253)
    * [smdet] add tal_cr and custompan with transfomer, add box_distribution to pred reg_max, test=document_fix
    
    * add tal_cr

--- ppdet/modeling/assigners/__init__.py
+++ ppdet/modeling/assigners/__init__.py
@@ -19,6 +19,7 @@ from . import simota_assigner
 from . import max_iou_assigner
 from . import fcosr_assigner
 from . import rotated_task_aligned_assigner
+from . import task_aligned_assigner_cr
 
 from .utils import *
 from .task_aligned_assigner import *
@@ -27,3 +28,4 @@ from .simota_assigner import *
 from .max_iou_assigner import *
 from .fcosr_assigner import *
 from .rotated_task_aligned_assigner import *
+from .task_aligned_assigner_cr import *
