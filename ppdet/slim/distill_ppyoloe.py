980bb8db577243d0f6a41e9061f08a563474e587
nemonameless <nemonameless@qq.com>
2022-11-22 11:22:22 +0000

    fix teacher_model

--- ppdet/slim/distill_ppyoloe.py
+++ ppdet/slim/distill_ppyoloe.py
@@ -53,6 +53,8 @@ class PPYOLOEDistillModel(nn.Layer):
 
     def forward(self, inputs, alpha=0.125, beta=1.5):
         if self.training:
+            with paddle.no_grad():
+                teacher_out = self.teacher_model(inputs)
             if hasattr(self.teacher_model.yolo_head, "assigned_labels"):
                 self.student_model.yolo_head.assigned_labels, self.student_model.yolo_head.assigned_bboxes, self.student_model.yolo_head.assigned_scores, self.student_model.yolo_head.mask_positive = \
                     self.teacher_model.yolo_head.assigned_labels, self.teacher_model.yolo_head.assigned_bboxes, self.teacher_model.yolo_head.assigned_scores, self.teacher_model.yolo_head.mask_positive
