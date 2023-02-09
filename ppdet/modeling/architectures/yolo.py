803fdbd3968cdb6d158cfeb29c48e446dc3882b6
xs1997zju <zxs19972022@163.com>
2023-02-09 18:55:48 +0800

    fix yolo extra_data process

--- ppdet/modeling/architectures/yolo.py
+++ ppdet/modeling/architectures/yolo.py
@@ -99,15 +99,6 @@ class YOLOv3(BaseArch):
 
         else:
             yolo_head_outs = self.yolo_head(neck_feats)
-            if self.use_extra_data:
-                extra_data = {} # record the bbox output before nms, such like scores and nms_keep_idx
-                """extra_data:{
-                            'scores': predict scores,
-                            'nms_keep_idx': bbox index before nms,
-                           }
-                """
-                extra_data['scores'] = yolo_head_outs[0]  # predict scores (probability)
-                # Todo: get logits output
 
             if self.for_mot:
                 # the detection part of JDE MOT model
@@ -123,23 +114,29 @@ class YOLOv3(BaseArch):
             else:
                 if self.return_idx:
                     # the detection part of JDE MOT model
-                    _, bbox, bbox_num, _ = self.post_process(
+                    _, bbox, bbox_num, nms_keep_idx = self.post_process(
                         yolo_head_outs, self.yolo_head.mask_anchors)
                 elif self.post_process is not None:
                     # anchor based YOLOs: YOLOv3,PP-YOLO,PP-YOLOv2 use mask_anchors
                     bbox, bbox_num, nms_keep_idx = self.post_process(
                         yolo_head_outs, self.yolo_head.mask_anchors,
                         self.inputs['im_shape'], self.inputs['scale_factor'])
-                    if self.use_extra_data:
-                        extra_data['nms_keep_idx'] = nms_keep_idx
-                        #Todo support for mask_anchors yolo
                 else:
                     # anchor free YOLOs: PP-YOLOE, PP-YOLOE+
                     bbox, bbox_num, nms_keep_idx = self.yolo_head.post_process(
                         yolo_head_outs, self.inputs['scale_factor'])
-                    if self.use_extra_data:
-                        extra_data['nms_keep_idx'] = nms_keep_idx
+
                 if self.use_extra_data:
+                    extra_data = {}  # record the bbox output before nms, such like scores and nms_keep_idx
+                    """extra_data:{
+                                'scores': predict scores,
+                                'nms_keep_idx': bbox index before nms,
+                               }
+                    """
+                    extra_data['scores'] = yolo_head_outs[0]  # predict scores (probability)
+                    # Todo: get logits output
+                    extra_data['nms_keep_idx'] = nms_keep_idx
+                    # Todo support for mask_anchors yolo
                     output = {'bbox': bbox, 'bbox_num': bbox_num, 'extra_data': extra_data}
                 else:
                     output = {'bbox': bbox, 'bbox_num': bbox_num}
