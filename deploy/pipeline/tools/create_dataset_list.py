009288db8e0b6f2f7b858e6300dc8595313c7261
LokeZhou <aishenghuoaiqq@163.com>
2023-02-17 02:42:28 +0000

    add licensed to lane_to_mask.py

--- deploy/pipeline/tools/create_dataset_list.py
+++ deploy/pipeline/tools/create_dataset_list.py
@@ -1,5 +1,5 @@
 # coding: utf8
-# Copyright (c) 2019 PaddlePaddle Authors. All Rights Reserve.
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserve.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
