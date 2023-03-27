30379dcccaae4e2c5297b966b3917a62f4474862
LokeZhou <aishenghuoaiqq@163.com>
2023-03-27 05:10:36 +0000

    [TIPC] * benchamrk use cocomini dataset * benchamrk set run 5m finish * static train pop data['image_file']

--- test_tipc/prepare.sh
+++ test_tipc/prepare.sh
@@ -99,9 +99,9 @@ elif [ ${MODE} = "benchmark_train" ];then
         cd ../../
     else
         # prepare lite benchmark coco data
-        wget -nc -P ./dataset/coco/ https://paddledet.bj.bcebos.com/data/coco_benchmark.tar --no-check-certificate
-        cd ./dataset/coco/ && tar -xf coco_benchmark.tar
-        mv -u coco_benchmark/* ./
+        wget -nc -P ./dataset/coco/ https://bj.bcebos.com/v1/paddledet/data/cocomini.zip --no-check-certificate
+        cd ./dataset/coco/ && unzip cocomini.zip
+        mv -u cocomini/* ./
         ls ./
         cd ../../
         # prepare lite benchmark mot data
