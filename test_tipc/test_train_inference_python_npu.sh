b48972ea8a16c1def9c67c1765f8d364c38b2f45
duanyanhui <duanyanhui@baidu.com>
2023-05-06 15:11:05 +0800

    fix conflict

--- test_tipc/test_train_inference_python_npu.sh
+++ test_tipc/test_train_inference_python_npu.sh
@@ -40,6 +40,7 @@ FILENAME=$1
  sed -i "s/--slim_config _template_pact/ /g" $FILENAME
  sed -i "s/--slim_config _template_fpgm/ /g" $FILENAME
  sed -i "s/--slim_config _template_kl_quant/ /g" $FILENAME
+ sed -i "s/gpu_list:0|0,1/gpu_list:0/g" $FILENAME
  sed -i 's/\"gpu\"/\"npu\"/g' test_tipc/test_train_inference_python.sh
 
  # parser params
@@ -51,8 +52,8 @@ grep -n '.yml' $FILENAME  | cut -d ":" -f 1 \
 | while read line_num ; do 
     train_cmd=$(func_parser_value "${lines[line_num-1]}")
     trainer_config=$(func_parser_config ${train_cmd})
-    echo ${trainer_config}
     sed -i 's/use_gpu/use_npu/g' "$REPO_ROOT_PATH/$trainer_config"
+    sed -i 's/aligned: True/aligned: False/g' "$REPO_ROOT_PATH/$trainer_config"
     # fine use_gpu in those included yaml
     sub_datalinee=`cat $REPO_ROOT_PATH/$trainer_config`
     IFS=$'\n'
@@ -62,9 +63,10 @@ grep -n '.yml' $FILENAME  | cut -d ":" -f 1 \
         sub_config=${sub_lines[sub_line_num-1]} 
         dst=${#sub_config}-5
         sub_path=$(func_parser_dir "${trainer_config}")
-        sub_config_path="${REPO_ROOT_PATH}${sub_path}/${sub_config:3:${dst}}"
-        echo ${sub_config_path}
+        sub_config_name=$(echo "$sub_config" | awk -F"'" '{ print $2 }')
+        sub_config_path="${REPO_ROOT_PATH}${sub_path}/${sub_config_name}"
         sed -i 's/use_gpu/use_npu/g' "$sub_config_path"
+        sed -i 's/aligned: True/aligned: False/g' "$sub_config_path"
     done
 done
 # pass parameters to test_train_inference_python.sh
