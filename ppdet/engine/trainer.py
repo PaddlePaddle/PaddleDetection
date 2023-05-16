a5183c7f7f8456c4a836ff1469d2e822d267f73e
duanyanhui <duanyanhui@baidu.com>
2023-05-16 14:19:30 +0800

    fix syn_bn for npu

--- ppdet/engine/trainer.py
+++ ppdet/engine/trainer.py
@@ -429,10 +429,8 @@ class Trainer(object):
         model = self.model
         if self.cfg.get('to_static', False):
             model = apply_to_static(self.cfg, model)
-        sync_bn = (
-            getattr(self.cfg, 'norm_type', None) == 'sync_bn' and
-            (self.cfg.use_gpu or self.cfg.use_npu or self.cfg.use_mlu) and
-            self._nranks > 1)
+        sync_bn = (getattr(self.cfg, 'norm_type', None) == 'sync_bn' and
+                   (self.cfg.use_gpu or self.cfg.use_mlu) and self._nranks > 1)
         if sync_bn:
             model = paddle.nn.SyncBatchNorm.convert_sync_batchnorm(model)
 
