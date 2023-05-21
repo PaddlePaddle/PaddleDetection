764afb531ea11d845e76b58e2da0faacf5b0a7b5
Atlantisming <505642573@qq.com>
2023-05-21 20:33:41 +0800

    test.

--- ppdet/metrics/coco_utils.py
+++ ppdet/metrics/coco_utils.py
@@ -186,3 +186,120 @@ def json_eval_results(metric, json_directory, dataset):
             cocoapi_eval(v_json, coco_eval_style[i], anno_file=anno_file)
         else:
             logger.info("{} not exists!".format(v_json))
+
+def zeroshot_cocoapi_eval(jsonfile,
+                 style,
+                 coco_gt=None,
+                 anno_file=None,
+                 max_dets=(100, 300, 1000),
+                 classwise=False,
+                 sigmas=None,
+                 use_area=True):
+    """
+    Args:
+        jsonfile (str): Evaluation json file, eg: bbox.json, mask.json.
+        style (str): COCOeval style, can be `bbox` , `segm` , `proposal`, `keypoints` and `keypoints_crowd`.
+        coco_gt (str): Whether to load COCOAPI through anno_file,
+                 eg: coco_gt = COCO(anno_file)
+        anno_file (str): COCO annotations file.
+        max_dets (tuple): COCO evaluation maxDets.
+        classwise (bool): Whether per-category AP and draw P-R Curve or not.
+        sigmas (nparray): keypoint labelling sigmas.
+        use_area (bool): If gt annotations (eg. CrowdPose, AIC)
+                         do not have 'area', please set use_area=False.
+    """
+    assert coco_gt != None or anno_file != None
+    if style == 'keypoints_crowd':
+        # please install xtcocotools==1.6
+        from xtcocotools.coco import COCO
+        from xtcocotools.cocoeval import COCOeval
+    else:
+        from pycocotools.coco import COCO
+        from pycocotools.cocoeval import COCOeval
+
+    unseen_list = [4, 5, 11, 12, 15, 16, 21, 23, 27, 29, 32, 34, 45, 47, 54,
+                   58, 63]
+    if coco_gt == None:
+        coco_gt = COCO(anno_file)
+    logger.info("Start evaluate...")
+    coco_dt = coco_gt.loadRes(jsonfile)
+    if style == 'proposal':
+        coco_eval = COCOeval(coco_gt, coco_dt, 'bbox')
+        coco_eval.params.useCats = 0
+        coco_eval.params.maxDets = list(max_dets)
+    elif style == 'keypoints_crowd':
+        coco_eval = COCOeval(coco_gt, coco_dt, style, sigmas, use_area)
+    else:
+        coco_eval = COCOeval(coco_gt, coco_dt, style)
+    coco_eval.evaluate()
+    coco_eval.accumulate()
+
+    coco_eval.summarize()
+
+    precisions = coco_eval.eval["precision"]
+
+    results_seen = []
+    results_unseen = []
+    for idx in range(precisions.shape[-3]):
+        # area range index 0: all area ranges
+        # max dets index -1: typically 100 per image
+        precision = precisions[0, :, idx, 0, -1]
+        precision = precision[precision > -1]
+        if precision.size:
+            ap = np.mean(precision)
+            # print(f"AP {idx}: {ap}")
+            if idx not in unseen_list:
+                results_seen.append(float(ap * 100))
+            else:
+                results_unseen.append(float(ap * 100))
+    print(f"bbox AP seen: {np.mean(results_seen)}")
+    print(f"bbox AP unseen: {np.mean(results_unseen)}")
+
+    if classwise:
+        # Compute per-category AP and PR curve
+        try:
+            from terminaltables import AsciiTable
+        except Exception as e:
+            logger.error(
+                'terminaltables not found, plaese install terminaltables. '
+                'for example: `pip install terminaltables`.')
+            raise e
+        precisions = coco_eval.eval['precision']
+        cat_ids = coco_gt.getCatIds()
+        # precision: (iou, recall, cls, area range, max dets)
+        assert len(cat_ids) == precisions.shape[2]
+        results_per_category = []
+        for idx, catId in enumerate(cat_ids):
+            # area range index 0: all area ranges
+            # max dets index -1: typically 100 per image
+            nm = coco_gt.loadCats(catId)[0]
+            precision = precisions[:, :, idx, 0, -1]
+            precision = precision[precision > -1]
+            if precision.size:
+                ap = np.mean(precision)
+            else:
+                ap = float('nan')
+            results_per_category.append(
+                (str(nm["name"]), '{:0.3f}'.format(float(ap))))
+            pr_array = precisions[0, :, idx, 0, 2]
+            recall_array = np.arange(0.0, 1.01, 0.01)
+            draw_pr_curve(
+                pr_array,
+                recall_array,
+                out_dir=style + '_pr_curve',
+                file_name='{}_precision_recall_curve.jpg'.format(nm["name"]))
+
+        num_columns = min(6, len(results_per_category) * 2)
+        results_flatten = list(itertools.chain(*results_per_category))
+        headers = ['category', 'AP'] * (num_columns // 2)
+        results_2d = itertools.zip_longest(
+            *[results_flatten[i::num_columns] for i in range(num_columns)])
+        table_data = [headers]
+        table_data += [result for result in results_2d]
+        table = AsciiTable(table_data)
+        logger.info('Per-category of {} AP: \n{}'.format(style, table.table))
+        logger.info("per-category PR curve has output to {} folder.".format(
+            style + '_pr_curve'))
+    # flush coco evaluation result
+    sys.stdout.flush()
+    return coco_eval.stats
---
+++ ppdet/modeling/embedder/clip/clip_models.py
@@ -0,0 +1,371 @@
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This code is based on: https://github.com/google-research/scenic/tree/main/scenic/projects/owl_vit
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import OrderedDict
+import numpy as np
+
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+from paddle import ParamAttr
+from paddle.nn.initializer import Normal, Constant, Assign
+
+from ppdet.modeling.layers import MultiHeadAttention
+from ppdet.modeling.initializer import zeros_, normal_
+from ppdet.core.workspace import register
+
+from .models import ModifiedResNet, ViT
+from .layers import Transformer, LayerNorm, QuickGELU, AttentionPool2D
+from .tokenizer import tokenize
+
+
+@register
+class CLIP(nn.Layer):
+    def __init__(self,
+                 embed_dim=512,
+                 # vision
+                 image_resolution=224,
+                 vision_layers=12,
+                 vision_width=768,
+                 vision_patch_size=32,
+                 # text
+                 context_length=77,
+                 vocab_size=49408,
+                 transformer_width=512,
+                 transformer_heads=8,
+                 transformer_layers=12,
+                 ):
+        super().__init__()
+        self.context_length = context_length
+        self.embed_dim = embed_dim
+
+        if isinstance(vision_layers, (tuple, list)):
+            vision_heads = vision_width * 32 // 64
+            self.visual = ModifiedResNet(
+                layers=vision_layers,
+                output_dim=embed_dim,
+                heads=vision_heads,
+                input_resolution=image_resolution,
+                width=vision_width
+            )
+        else:
+            vision_heads = vision_width // 64
+            self.visual = ViT(
+                input_resolution=image_resolution,
+                patch_size=vision_patch_size,
+                width=vision_width,
+                layers=vision_layers,
+                heads=vision_heads,
+                output_dim=embed_dim
+            )
+
+        self.transformer = Transformer(
+            width=transformer_width,
+            layers=transformer_layers,
+            heads=transformer_heads,
+            attn_mask=self.build_attention_mask()
+        )
+
+        self.vocab_size = vocab_size
+        self.token_embedding = nn.Embedding(vocab_size, transformer_width)
+
+        positional_embedding = self.create_parameter(
+            shape=(self.context_length, transformer_width),
+            default_initializer=Assign(
+                paddle.empty((self.context_length, transformer_width))
+            )
+        )
+        self.add_parameter("positional_embedding", positional_embedding)
+
+        self.ln_final = nn.LayerNorm(transformer_width)
+
+        text_projection = self.create_parameter(
+            shape=(transformer_width, embed_dim),
+            default_initializer=Assign(
+                paddle.empty((transformer_width, embed_dim))
+            )
+        )
+        self.add_parameter("text_projection", text_projection)
+        # self.text_projection = nn.Linear(
+        #     transformer_width, embed_dim, bias_attr=False)
+
+        logit_scale = self.create_parameter(
+            shape=(1,),
+            default_initializer=Assign(paddle.ones([1]))
+        )
+        self.add_parameter("logit_scale", logit_scale)
+
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
+        Normal(std=0.02)(self.token_embedding.weight)
+        Normal(std=0.01)(self.positional_embedding)
+
+        if isinstance(self.visual, ModifiedResNet):
+            if self.visual.attnpool is not None:
+                std = self.embed_dim ** -0.5
+                # normal_ = Normal(std=std)
+                normal_(self.visual.attnpool.attn.q_proj.weight, std=std)
+                normal_(self.visual.attnpool.attn.k_proj.weight, std=std)
+                normal_(self.visual.attnpool.attn.v_proj.weight, std=std)
+                normal_(self.visual.attnpool.attn.out_proj.weight, std=std)
+
+            for resnet_block in [self.visual.layer1, self.visual.layer2, self.visual.layer3, self.visual.layer4]:
+                for name, param in resnet_block.named_parameters():
+                    if name.endswith("bn3.weight"):
+                        Constant(value=0.0)(param)
+
+        proj_std = (self.transformer.width ** -0.5) * \
+            ((2 * self.transformer.layers) ** -0.5)
+        attn_std = self.transformer.width ** -0.5
+        fc_std = (2 * self.transformer.width) ** -0.5
+
+        for resblock in self.transformer.resblocks:
+            # normal_ = Normal(std=attn_std)
+            normal_(resblock.attn.q_proj.weight, std=attn_std)
+            normal_(resblock.attn.k_proj.weight, std=attn_std)
+            normal_(resblock.attn.v_proj.weight, std=attn_std)
+            normal_(resblock.attn.out_proj.weight, std=proj_std)
+            normal_(resblock.mlp.c_fc.weight, std=fc_std)
+            normal_(resblock.mlp.c_proj.weight, std=proj_std)
+
+        if self.text_projection is not None:
+            Normal(std=self.transformer.width ** -0.5)(self.text_projection)
+
+    def build_attention_mask(self):
+        mask = paddle.full(
+            (self.context_length, self.context_length), float("-inf")
+        )
+        mask = paddle.triu(mask, diagonal=1)
+        return mask
+
+    def encode_image(self, image):
+        return self.visual(image)
+
+    def encode_text(self, text):
+        x = self.token_embedding(text)
+        x = x + self.positional_embedding
+        x = self.transformer(x)
+        x = self.ln_final(x)
+
+        select = []
+        index = zip(
+            paddle.arange(x.shape[0]).numpy(),
+            text.argmax(axis=-1).numpy()
+        )
+        for i, j in index:
+            select.append(x[int(i), int(j)])
+
+        x = paddle.stack(select) @ self.text_projection
+
+        return x
+
+    def forward(self, image, text):
+        image_features = self.encode_image(image)
+        text_features = self.encode_text(text)
+
+        # normalized features
+        image_features = image_features / \
+            image_features.norm(axis=-1, keepdim=True)
+        text_features = text_features / \
+            text_features.norm(axis=-1, keepdim=True)
+
+        # cosine similarity as logits
+        logit_scale = self.logit_scale.exp()
+        logits_per_image = logit_scale * image_features @ text_features.t()
+        logits_per_text = logit_scale * text_features @ image_features.t()
+
+        # shape = [global_batch_size, global_batch_size]
+        return logits_per_image, logits_per_text
+
+COCO_CATEGORIES = {
+    1: "person",
+    2: "bicycle",
+    3: "car",
+    4: "motorcycle",
+    5: "airplane",
+    6: "bus",
+    7: "train",
+    8: "truck",
+    9: "boat",
+    10: "traffic light",
+    11: "fire hydrant",
+    12: "street sign",
+    13: "stop sign",
+    14: "parking meter",
+    15: "bench",
+    16: "bird",
+    17: "cat",
+    18: "dog",
+    19: "horse",
+    20: "sheep",
+    21: "cow",
+    22: "elephant",
+    23: "bear",
+    24: "zebra",
+    25: "giraffe",
+    26: "hat",
+    27: "backpack",
+    28: "umbrella",
+    29: "shoe",
+    30: "eye glasses",
+    31: "handbag",
+    32: "tie",
+    33: "suitcase",
+    34: "frisbee",
+    35: "skis",
+    36: "snowboard",
+    37: "sports ball",
+    38: "kite",
+    39: "baseball bat",
+    40: "baseball glove",
+    41: "skateboard",
+    42: "surfboard",
+    43: "tennis racket",
+    44: "bottle",
+    45: "plate",
+    46: "wine glass",
+    47: "cup",
+    48: "fork",
+    49: "knife",
+    50: "spoon",
+    51: "bowl",
+    52: "banana",
+    53: "apple",
+    54: "sandwich",
+    55: "orange",
+    56: "broccoli",
+    57: "carrot",
+    58: "hot dog",
+    59: "pizza",
+    60: "donut",
+    61: "cake",
+    62: "chair",
+    63: "couch",
+    64: "potted plant",
+    65: "bed",
+    66: "mirror",
+    67: "dining table",
+    68: "window",
+    69: "desk",
+    70: "toilet",
+    71: "door",
+    72: "tv",
+    73: "laptop",
+    74: "computer mouse",
+    75: "remote",
+    76: "keyboard",
+    77: "cell phone",
+    78: "microwave",
+    79: "oven",
+    80: "toaster",
+    81: "sink",
+    82: "refrigerator",
+    83: "blender",
+    84: "book",
+    85: "clock",
+    86: "vase",
+    87: "scissors",
+    88: "teddy bear",
+    89: "hair drier",
+    90: "toothbrush",
+    91: "hair brush",
+}
+
+def article(name):
+    return "an" if name[0] in "aeiou" else "a"
+
+
+def processed_name(name, rm_dot=False):
+    # _ for lvis
+    # / for obj365
+    res = name.replace("_", " ").replace("/", " or ").lower()
+    if rm_dot:
+        res = res.rstrip(".")
+    return res
+
+
+single_template = ["a photo of a {}."]
+
+multiple_templates = [
+    "There is {article} {} in the scene.",
+    "There is the {} in the scene.",
+    "a photo of {article} {} in the scene.",
+    "a photo of the {} in the scene.",
+    "a photo of one {} in the scene.",
+    "itap of {article} {}.",
+    "itap of my {}.",  # itap: I took a picture of
+    "itap of the {}.",
+    "a photo of {article} {}.",
+    "a photo of my {}.",
+    "a photo of the {}.",
+    "a photo of one {}.",
+    "a photo of many {}.",
+    "a good photo of {article} {}.",
+    "a good photo of the {}.",
+    "a bad photo of {article} {}.",
+    "a bad photo of the {}.",
+    "a photo of a nice {}.",
+    "a photo of the nice {}.",
+    "a photo of a cool {}.",
+    "a photo of the cool {}.",
+    "a photo of a weird {}.",
+    "a photo of the weird {}.",
+    "a photo of a small {}.",
+    "a photo of the small {}.",
+    "a photo of a large {}.",
+    "a photo of the large {}.",
+    "a photo of a clean {}.",
+    "a photo of the clean {}.",
+    "a photo of a dirty {}.",
+    "a photo of the dirty {}.",
+    "a bright photo of {article} {}.",
+    "a bright photo of the {}.",
+    "a dark photo of {article} {}.",
+    "a dark photo of the {}.",
+    "a photo of a hard to see {}.",
+    "a photo of the hard to see {}.",
+    "a low resolution photo of {article} {}.",
+    "a low resolution photo of the {}.",
+    "a cropped photo of {article} {}.",
+    "a cropped photo of the {}.",
+    "a close-up photo of {article} {}.",
+    "a close-up photo of the {}.",
+    "a jpeg corrupted photo of {article} {}.",
+    "a jpeg corrupted photo of the {}.",
+    "a blurry photo of {article} {}.",
+    "a blurry photo of the {}.",
+    "a pixelated photo of {article} {}.",
+    "a pixelated photo of the {}.",
+    "a black and white photo of the {}.",
+    "a black and white photo of {article} {}.",
+    "a plastic {}.",
+    "the plastic {}.",
+    "a toy {}.",
+    "the toy {}.",
+    "a plushie {}.",
+    "the plushie {}.",
+    "a cartoon {}.",
+    "the cartoon {}.",
+    "an embroidered {}.",
+    "the embroidered {}.",
+    "a painting of the {}.",
+    "a painting of a {}.",
+]
