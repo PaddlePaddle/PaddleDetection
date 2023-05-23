a8eef9bea6194c4117c530a62a849273058c1b6d
Atlantisming <505642573@qq.com>
2023-05-23 14:53:12 +0800

    add ov-detr

--- ppdet/modeling/necks/channel_mapper.py
+++ ppdet/modeling/necks/channel_mapper.py
@@ -1,15 +1,15 @@
-# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved. 
-#   
-# Licensed under the Apache License, Version 2.0 (the "License");   
-# you may not use this file except in compliance with the License.  
-# You may obtain a copy of the License at   
-#   
-#     http://www.apache.org/licenses/LICENSE-2.0    
-#   
-# Unless required by applicable law or agreed to in writing, software   
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
-# See the License for the specific language governing permissions and   
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 """
 this code is base on mmdet: git@github.com:open-mmlab/mmdetection.git
@@ -21,7 +21,8 @@ from ..backbones.hrnet import ConvNormLayer
 from ..shape_spec import ShapeSpec
 from ..initializer import xavier_uniform_, constant_
 
-__all__ = ['ChannelMapper']
+
+__all__ = ['ChannelMapper', 'DETRChannelMapper']
 
 
 @register
@@ -46,7 +47,7 @@ class ChannelMapper(nn.Layer):
             would be extra_convs when num_outs larger than the length
             of in_channels.
         init_cfg (dict or list[dict], optional): Initialization config dict.
-        
+
     """
 
     def __init__(self,
@@ -120,3 +121,64 @@ class ChannelMapper(nn.Layer):
                 xavier_uniform_(p)
                 if hasattr(p, 'bias') and p.bias is not None:
                     constant_(p.bais)
+
+@register
+class DETRChannelMapper(nn.Layer):
+    __shared__ = ['hidden_dim']
+    def __init__(self,
+                 backbone_num_channels=[512, 1024, 2048],
+                 hidden_dim=256,
+                 num_feature_levels=4,
+                 weight_attr=None,
+                 bias_attr=None,
+                 ):
+        super(DETRChannelMapper, self).__init__()
+        assert len(backbone_num_channels) <= num_feature_levels
+        self.num_feature_levels = num_feature_levels
+        self.input_proj = nn.LayerList()
+        for in_channels in backbone_num_channels:
+            self.input_proj.append(
+                nn.Sequential(
+                    nn.Conv2D(
+                        in_channels,
+                        hidden_dim,
+                        kernel_size=1,
+                        weight_attr=weight_attr,
+                        bias_attr=bias_attr),
+                    nn.GroupNorm(32, hidden_dim)))
+        in_channels = backbone_num_channels[-1]
+        for _ in range(num_feature_levels - len(backbone_num_channels)):
+            self.input_proj.append(
+                nn.Sequential(
+                    nn.Conv2D(
+                        in_channels,
+                        hidden_dim,
+                        kernel_size=3,
+                        stride=2,
+                        padding=1,
+                        weight_attr=weight_attr,
+                        bias_attr=bias_attr),
+                    nn.GroupNorm(32, hidden_dim)))
+            in_channels = hidden_dim
+
+    def _reset_parameters(self):
+        for l in self.input_proj:
+            xavier_uniform_(l[0].weight)
+            constant_(l[0].bias)
+
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        return {'backbone_num_channels': [i.channels for i in input_shape], }
+
+    def forward(self, src_feats):
+        srcs = []
+        for i in range(len(src_feats)):
+            srcs.append(self.input_proj[i](src_feats[i]))
+        if self.num_feature_levels > len(srcs):
+            len_srcs = len(srcs)
+            for i in range(len_srcs, self.num_feature_levels):
+                if i == len_srcs:
+                    srcs.append(self.input_proj[i](src_feats[-1]))
+                else:
+                    srcs.append(self.input_proj[i](srcs[-1]))
+        return srcs
