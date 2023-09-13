9697fd6a4dce9cabb4f9905546de5e9035961668
Yang Nie <flytocc@outlook.com>
2023-09-13 14:32:56 +0800

    Revert "fix eval during training"
    This reverts commit 3ee85c54d0e2db321159aa9b3930d1a997591f59.

--- ppdet/metrics/metrics.py
+++ ppdet/metrics/metrics.py
@@ -124,7 +124,7 @@ class COCOMetric(Metric):
 
     def accumulate(self):
         if len(self.results['bbox']) > 0:
-            output = f"bbox_{paddle.distributed.get_rank()}.json"
+            output = "bbox.json"
             if self.output_eval:
                 output = os.path.join(self.output_eval, output)
             with open(output, 'w') as f:
