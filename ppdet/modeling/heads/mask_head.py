7c73cb078c771f805e2436b7b578890211a384ce
Wang Huan <wanghuan29@baidu.com>
2024-04-12 08:48:50 +0000

    refine

--- ppdet/modeling/heads/mask_head.py
+++ ppdet/modeling/heads/mask_head.py
@@ -229,7 +229,7 @@ class MaskHead(nn.Layer):
                 mask_out = mask_logit[index, labels]
                 mask_out_shape = mask_out.shape
                 mask_out = paddle.reshape(mask_out, 
-                    index.shape + mask_out_shape[-2] + mask_out_shape[-1])
+                    index.shape + [mask_out_shape[-2]] + [mask_out_shape[-1]])
                 mask_out = F.sigmoid(mask_out)
         return mask_out
 
