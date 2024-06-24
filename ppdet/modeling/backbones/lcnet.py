f0630f0c6f14b967955d0bb99a37dfe3216911b7
LielinJiang <jianglielin@baidu.com>
2024-06-24 06:26:00 +0000

    support picodet run on npu

--- ppdet/modeling/backbones/lcnet.py
+++ ppdet/modeling/backbones/lcnet.py
@@ -19,7 +19,7 @@ from __future__ import print_function
 import paddle
 import paddle.nn as nn
 from paddle import ParamAttr
-from paddle.nn import AdaptiveAvgPool2D, Conv2D
+from paddle.nn import Conv2D
 from paddle.regularizer import L2Decay
 from paddle.nn.initializer import KaimingNormal
 
@@ -131,6 +131,32 @@ class DepthwiseSeparable(nn.Layer):
         x = self.pw_conv(x)
         return x
 
+class AdaptiveAvgPool2D(nn.AdaptiveAvgPool2D):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if paddle.device.get_device().startswith("npu"):
+            self.device = "npu"
+        else:
+            self.device = None
+
+        if isinstance(self._output_size, int) and self._output_size == 1:
+            self._gap = True
+        elif isinstance(self._output_size, tuple) and self._output_size[
+                0] == 1 and self._output_size[1] == 1:
+            self._gap = True
+        else:
+            self._gap = False
+
+    def forward(self, x):
+        if self.device == "npu" and self._gap:
+            # Global Average Pooling
+            N, C, _, _ = x.shape
+            x_mean = paddle.mean(x, axis=[2, 3])
+            x_mean = paddle.reshape(x_mean, [N, C, 1, 1])
+            return x_mean
+        else:
+            return super().forward(x)
 
 class SEModule(nn.Layer):
     def __init__(self, channel, reduction=4):
