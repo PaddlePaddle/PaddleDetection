986101558b0bdbf0c242a586bfe5e3f3b2196ec2
LielinJiang <50691816+LielinJiang@users.noreply.github.com>
2024-06-24 15:35:05 +0800

    support picodet run on npu (#9037)

--- tools/train.py
+++ tools/train.py
@@ -28,7 +28,11 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import paddle
-
+if paddle.device.get_device().startswith("npu"):
+    import cv2
+    cv2.setNumThreads(0)
+    cv2.ocl.setUseOpenCL(False)
+    
 from ppdet.core.workspace import load_config, merge_config
 
 from ppdet.engine import Trainer, TrainerCot, init_parallel_env, set_random_seed, init_fleet_env
