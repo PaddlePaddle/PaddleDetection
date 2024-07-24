8db68a7ebefb7adc0a96266da59da0427cf8a1ed
lyuwenyu <wenyu.lyu@gmail.com>
2024-07-24 07:52:25 +0000

    update

--- deploy/python/infer.py
+++ deploy/python/infer.py
@@ -994,9 +994,20 @@ def load_predictor(model_dir,
             "Predict by TensorRT mode: {}, expect device=='GPU', but device == {}"
             .format(run_mode, device))
 
-    # TODO
-    config = Config(model_dir, 'model')
-    
+    model_path = model_dir
+    model_prefix = 'model'
+    infer_param = os.path.join(model_dir, 'model.pdiparams')
+    if not os.path.exists(infer_param):
+        model_prefix = 'inference'
+        if paddle.framework.use_pir_api():
+            infer_model = os.path.join(model_dir, 'inference.pdmodel')
+        else:
+            infer_model = os.path.join(model_dir, 'inference.json')
+        if not os.path.exists(infer_model):
+            raise ValueError(
+                "Cannot find any inference model in dir: {}.".format(model_dir))
+    config = Config(model_path, model_prefix)
+
     if device == 'GPU':
         # initial GPU memory(M), device ID
         config.enable_use_gpu(200, 0)
