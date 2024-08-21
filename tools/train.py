cedee42ef0948db9bfe74e2f163b06388d14d816
LielinJiang <jianglielin@baidu.com>
2024-08-21 09:42:36 +0000

    adapt multi devices to develop

--- tools/train.py
+++ tools/train.py
@@ -27,12 +27,11 @@ sys.path.insert(0, parent_path)
 import warnings
 warnings.filterwarnings('ignore')
 
+import cv2
+cv2.setNumThreads(0)
+cv2.ocl.setUseOpenCL(False)
+
 import paddle
-if paddle.device.get_device().startswith("npu"):
-    import cv2
-    cv2.setNumThreads(0)
-    cv2.ocl.setUseOpenCL(False)
-    
 from ppdet.core.workspace import load_config, merge_config
 
 from ppdet.engine import Trainer, TrainerCot, init_parallel_env, set_random_seed, init_fleet_env
