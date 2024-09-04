96483f21bcecae8cf460b792963ff24393e23a5e
zhangyubo0722 <94225063+zhangyubo0722@users.noreply.github.com>
2024-09-04 22:27:06 +0800

    add_Mask_RT-DETR_models (#9119)

--- ppdet/modeling/backbones/hgnet_v2.py
+++ ppdet/modeling/backbones/hgnet_v2.py
@@ -337,6 +337,26 @@ class PPHGNetV2(nn.Layer):
     """
 
     arch_configs = {
+        'S': {
+            'stem_channels': [3, 24, 32],
+            'stage_config': {
+                # in_channels, mid_channels, out_channels, num_blocks, downsample, light_block, kernel_size, layer_num
+                "stage1": [32, 32, 64, 1, False, False, 3, 3],
+                "stage2": [64, 48, 256, 1, True, False, 3, 3],
+                "stage3": [256, 96, 512, 2, True, True, 5, 3],
+                "stage4": [512, 192, 1024, 1, True, True, 5, 3],
+            }
+        },
+        'M': {
+            'stem_channels': [3, 24, 32],
+            'stage_config': {
+                # in_channels, mid_channels, out_channels, num_blocks, downsample, light_block, kernel_size, layer_num
+                "stage1": [32, 32, 96, 1, False, False, 3, 4],
+                "stage2": [96, 64, 384, 1, True, False, 3, 4],
+                "stage3": [384, 128, 768, 3, True, True, 5, 4],
+                "stage4": [768, 256, 1536, 1, True, True, 5, 4],
+            }
+        },
         'L': {
             'stem_channels': [3, 32, 48],
             'stage_config': {
